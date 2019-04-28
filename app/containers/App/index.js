ea3dd541bb8dc0bf176308062f3d5339282ac336
Thaman <thaman.chand@invian.fi>
2019-04-28 19:27:14 +0300

    fix it

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -9,15 +9,13 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import HomePage from 'containers/HomePage/Loadable';
-import FeaturePage from 'containers/FeaturePage/Loadable';
-import NotFoundPage from 'containers/NotFoundPage/Loadable';
+import HomePage from 'containers/HomePage';
+import NotFoundPage from 'containers/NotFoundPage';
 
 export default function App() {
   return (
     <Switch>
       <Route exact path="/" component={HomePage} />
-      <Route path="/features" component={FeaturePage} />
       <Route path="" component={NotFoundPage} />
     </Switch>
   );
