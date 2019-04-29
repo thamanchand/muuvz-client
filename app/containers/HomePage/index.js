d6ca21a6bf9688e27afee76b4856ff89e745b39a
Thaman <thaman.chand@invian.fi>
2019-04-29 16:10:51 +0300

    render <Header> <Features> <Testimonial> <Footer> components in Homepage

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -1,11 +1,17 @@
 /*
  * HomePage
+ *
  */
 
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Container, Row, Col } from 'reactstrap';
 
+import Header from './components/Header';
+import Features from './components/Features';
+
+import Footer from './components/Footer';
+import Testimonials from './components/Testimonials';
 import logo from '../../assets/images/muverz.svg';
 
 const HomePage = () => (
@@ -34,6 +40,10 @@ const HomePage = () => (
         </Row>
       </Container>
     </div>
+    <Header />
+    <Features />
+    <Testimonials />
+    <Footer />
   </div>
 );
 
