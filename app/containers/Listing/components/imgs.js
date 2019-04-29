d3b19034e8e2710edf4663298620865ca6f5cd4e
Thaman <thaman.chand@invian.fi>
2019-04-29 21:59:04 +0300

    import dummy images reference

---
+++ app/containers/Listing/components/imgs.js
@@ -0,0 +1,5 @@
+import dog1 from '../../../assets/images/dummy-list/1.png';
+import dog2 from '../../../assets/images/dummy-list/2.png';
+import dog3 from '../../../assets/images/dummy-list/3.png';
+
+export default [{ src: dog1 }, { src: dog2 }, { src: dog3 }];
