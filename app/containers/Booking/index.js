40fccb8595eec511e22d5c15b2785a407a1d7cc8
Thaman <thaman.chand@invian.fi>
2019-05-01 22:39:11 +0300

    <Booking> component

--- app/containers/Booking/index.js
+++ app/containers/Booking/index.js
@@ -1,46 +1,42 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import { Col, Container, Row } from 'reactstrap';
-import PropTypes from 'prop-types';
 import Rating from './components/Rating';
 import TotalCustomers from './components/TotalCustomers';
 import TotalBookings from './components/TotalBookings';
 import Favourite from './components/Favourite';
 import Layout from '../Layout/index';
+import TimelineCalendar from './components/TimelineCalendar';
 
-class BookingDashboard extends PureComponent {
-  static propTypes = {
-    t: PropTypes.func.isRequired,
-  };
-
-  render() {
-    const { t } = this.props;
-
-    return (
-      <div>
-        <Layout />
-        <div className="container__wrap">
-          <Container className="dashboard container">
-            <Row>
-              <Col md={12}>
-                <h3 className="page-title">
-                  {t('dashboard_booking.page_title')}
-                </h3>
-              </Col>
-            </Row>
-            <Row>
-              <Rating />
-              <TotalBookings />
-              <TotalCustomers />
-              <Favourite />
-            </Row>
-            <Row>
-              <p>Test</p>
-            </Row>
-          </Container>
-        </div>
-      </div>
-    );
-  }
-}
+const BookingDashboard = () => (
+  <div>
+    <Layout />
+    <div className="container__wrap">
+      <Container className="dashboard container">
+        <Row>
+          <Col md={12}>
+            <h3 className="page-title">Booking</h3>
+          </Col>
+        </Row>
+        <Row>
+          <Rating />
+          <TotalBookings />
+          <TotalCustomers />
+          <Favourite />
+        </Row>
+        <Row>
+          <div className="container">
+            <Container className="dashboard">
+              <Row>
+                <Col md={12}>
+                  <TimelineCalendar />
+                </Col>
+              </Row>
+            </Container>
+          </div>
+        </Row>
+      </Container>
+    </div>
+  </div>
+);
 
 export default BookingDashboard;
