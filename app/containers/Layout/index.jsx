960dec6128b606041761292e083766785ebd156f
Thaman <thaman.chand@invian.fi>
2019-05-01 21:04:20 +0300

    finalise <Layout> component by rendering <Topbar> and <Sidebar> as child components

---
+++ app/containers/Layout/index.jsx
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+import Topbar from './topbar/Topbar';
+import Sidebar from './sidebar/Sidebar';
+
+import { changeMobileSidebarVisibility, changeSidebarVisibility } from './action';
+
+class Layout extends Component {
+
+  changeSidebarVisibility = () => {
+    const { dispatch } = this.props;
+    dispatch(changeSidebarVisibility());
+  };
+
+  changeMobileSidebarVisibility = () => {
+    const { dispatch } = this.props;
+    dispatch(changeMobileSidebarVisibility());
+  };
+
+  render() {
+    const { sidebar } = this.props;
+    const layoutClass = classNames({
+      layout: true,
+      'layout--collapse': sidebar.collapse,
+    });
+
+    return (
+      <React.Fragment>
+        <div className={layoutClass}>
+          <Topbar
+            changeMobileSidebarVisibility={this.changeMobileSidebarVisibility}
+            changeSidebarVisibility={this.changeSidebarVisibility}
+          />
+          <Sidebar
+            sidebar={sidebar}
+            changeMobileSidebarVisibility={this.changeMobileSidebarVisibility}
+          />
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+Layout.propTypes = {
+  sidebar: PropTypes.shape({
+    show: PropTypes.bool,
+    collapse: PropTypes.bool,
+  }).isRequired,
+  dispatch: PropTypes.func.isRequired,
+};
+
+export default withRouter(connect(state => ({
+  sidebar: state.sidebar,
+}))(Layout));
