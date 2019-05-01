5370b90755c85e273c644696c4a0d0429757bf38
Thaman <thaman.chand@invian.fi>
2019-05-01 21:00:29 +0300

    set constants for layout component

---
+++ app/containers/Layout/constants.js
@@ -0,0 +1,9 @@
+/*
+ *
+ * Layout constants
+ *
+ */
+
+export const CHANGE_SIDEBAR_VISIBILITY = 'app/layout/CHANGE_SIDEBAR_VISIBILITY';
+export const CHANGE_MOBILE_SIDEBAR_VISIBILITY =
+  'app/layout/CHANGE_MOBILE_SIDEBAR_VISIBILITY';
