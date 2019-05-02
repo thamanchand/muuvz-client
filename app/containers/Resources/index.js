7057aaee9fee8074df9747eeb2ae4ec3a126beaf
Thaman <thaman.chand@invian.fi>
2019-05-02 13:44:04 +0300

    <Resources> page init and render <ResourceList> component

---
+++ app/containers/Resources/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Col, Container, Row } from 'reactstrap';
+
+import ResourceList from './components/ResourceList';
+import Statistics from '../Booking/components/Statistics';
+import Layout from '../Layout/index';
+
+const Resources = () => (
+  <div>
+    <Layout />
+    <div className="container__wrap">
+      <Container className="dashboard container">
+        <Row>
+          <Col md={12}>
+            <h3 className="page-title">Resources</h3>
+          </Col>
+        </Row>
+        <Row>
+          <Statistics />
+        </Row>
+        <Row>
+          <ResourceList />
+        </Row>
+      </Container>
+    </div>
+  </div>
+);
+
+export default Resources;
