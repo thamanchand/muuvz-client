a873c7de632eb8ddef35c9544fbb49e62d7929cb
Thaman <thaman.chand@invian.fi>
2019-05-05 09:17:59 +0300

    vanLoadWatcher to yield GET vans API

---
+++ app/containers/Listing/saga.js
@@ -0,0 +1,35 @@
+import { LOCATION_CHANGE } from 'react-router-redux';
+import {
+  call,
+  takeLatest,
+  take,
+  put,
+} from 'redux-saga/effects';
+
+// Utils
+import request from 'utils/request';
+
+// constants
+import {
+  onVanListLoadSuccess,
+  onVanListLoadFailure,
+} from './actions';
+
+import { ON_VANLIST_LOAD } from './contants';
+
+export function* vanLoadWatcher() {
+  try {
+    const requestURL = 'http://localhost:1337/vans';
+    const vanList = yield call(request, requestURL, { method: 'GET' });
+    if (vanList) {
+      yield put(onVanListLoadSuccess(vanList));
+    }
+  } catch(error) {
+    yield put(onVanListLoadFailure(error));
+  }
+}
+
+export default function* defaultSaga() {
+  yield takeLatest(ON_VANLIST_LOAD, vanLoadWatcher);
+  yield take(LOCATION_CHANGE);
+}
