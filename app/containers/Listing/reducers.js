ac240c39faf0c6344c938e5fb2bef4d412a2ecd9
Thaman <thaman.chand@invian.fi>
2019-05-05 09:11:58 +0300

    vanListReducer initialised

---
+++ app/containers/Listing/reducers.js
@@ -0,0 +1,41 @@
+/*
+ *
+ * AddTask reducer
+ *
+ */
+
+import { fromJS } from 'immutable';
+
+import {
+  ON_VANLIST_LOAD,
+  ON_VANLIST_LOAD_SUCCESS,
+  ON_VANLIST_LOAD_FAILURE,
+} from './contants';
+
+
+const initialState = fromJS({
+  vanList: [],
+  loading: false,
+});
+
+function vanListReducer(state = initialState, action) {
+  switch (action.type) {
+    case ON_VANLIST_LOAD:
+      return state
+        .set('vanList', fromJS([]))
+        .set('loading', true);
+
+    case ON_VANLIST_LOAD_SUCCESS:
+      return state
+        .set('vanList', action.vanList)
+        .set('isLoading', false);
+
+    case ON_VANLIST_LOAD_FAILURE:
+      return state.set('error', fromJS(action.error))
+
+    default:
+      return state;
+  }
+}
+
+export default vanListReducer;
