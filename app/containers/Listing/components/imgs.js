d772e62b266280526a59adbfe3d17745bdb3fd4e
Thaman <thaman.chand@invian.fi>
2019-05-05 09:31:13 +0300

    add id property to imgage object

--- app/containers/Listing/components/imgs.js
+++ app/containers/Listing/components/imgs.js
@@ -2,4 +2,4 @@ import dog1 from '../../../assets/images/dummy-list/1.png';
 import dog2 from '../../../assets/images/dummy-list/2.png';
 import dog3 from '../../../assets/images/dummy-list/3.png';
 
-export default [{ src: dog1 }, { src: dog2 }, { src: dog3 }];
+export default [{ src: dog1, id: 1 }, { src: dog2, id: 2 }, { src: dog3, id: 3 }];
