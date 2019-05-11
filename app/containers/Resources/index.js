a35c72ad531d3ecae4f12e2c31088b6cafd9e53e
Thaman <thaman.chand@invian.fi>
2019-05-11 20:59:58 +0300

    componentDidMount, withReducer, withSaga, withConnect

--- app/containers/Resources/index.js
+++ app/containers/Resources/index.js
@@ -1,16 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { bindActionCreators, compose } from 'redux';
+import { createStructuredSelector } from 'reselect';
 import { Col, Container, Row } from 'reactstrap';
+
+// Utils
+import injectSaga from 'utils/injectSaga';
+import injectReducer from 'utils/injectReducer';
 
 import ResourceList from './components/ResourceList';
 import Statistics from '../Booking/components/Statistics';
 import Layout from '../Layout/index';
 
-class Resources extends React.PureComponent {
-  componentDidMount() {
 
+import { onVanListLoad } from './actions';
+import { makeSelectVans } from './selectors';
+import saga from './saga';
+import reducer from './reducers';
+
+const key = 'resourcePage';
+
+
+class ResourcesPage extends React.PureComponent {
+  componentDidMount() {
+    this.props.onVanListLoad();
   }
 
   render() {
+    const { vanList } = this.props;
     return (
       <div>
         <Layout />
@@ -25,7 +43,7 @@ class Resources extends React.PureComponent {
               <Statistics />
             </Row>
             <Row>
-              <ResourceList />
+              <ResourceList vanList={vanList} />
             </Row>
           </Container>
         </div>
@@ -35,4 +53,46 @@ class Resources extends React.PureComponent {
 
 }
 
-export default Resources;
+
+ResourcesPage.propTypes = {
+  onVanListLoad: PropTypes.func,
+  vanList: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string,
+    createdAt: PropTypes.string,
+    brand: PropTypes.string,
+    cruisecontrol: PropTypes.bool,
+    exteriordimensions: PropTypes.string,
+    features: PropTypes.bool,
+    fueltype: PropTypes.string,
+    interiordimensions: PropTypes.string,
+    licensetype: PropTypes.string,
+    located: PropTypes.string,
+    mileage: PropTypes.number,
+    model: PropTypes.string,
+    passengernum: PropTypes.string,
+    transmission: PropTypes.string,
+    year: PropTypes.string,
+  })).isRequired,
+}
+
+const mapStateToProps = createStructuredSelector({
+  vanList: makeSelectVans(),
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  onVanListLoad: bindActionCreators(onVanListLoad, dispatch)
+});
+
+const withConnect = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+);
+
+const withReducer = injectReducer({ key, reducer });
+const withSaga = injectSaga({ key, saga });
+
+export default compose(
+  withReducer,
+  withSaga,
+  withConnect,
+)(ResourcesPage);
