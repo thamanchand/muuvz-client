dc1191bd222894d33266d3dc584d93e9bdb28b55
Thaman <thaman.chand@invian.fi>
2019-05-11 20:46:03 +0300

    modify functional component to PureComponent

--- app/containers/Resources/index.js
+++ app/containers/Resources/index.js
@@ -5,25 +5,34 @@ import ResourceList from './components/ResourceList';
 import Statistics from '../Booking/components/Statistics';
 import Layout from '../Layout/index';
 
-const Resources = () => (
-  <div>
-    <Layout />
-    <div className="container__wrap">
-      <Container className="dashboard container">
-        <Row>
-          <Col md={12}>
-            <h3 className="page-title">Resources</h3>
-          </Col>
-        </Row>
-        <Row>
-          <Statistics />
-        </Row>
-        <Row>
-          <ResourceList />
-        </Row>
-      </Container>
-    </div>
-  </div>
-);
+class Resources extends React.PureComponent {
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div>
+        <Layout />
+        <div className="container__wrap">
+          <Container className="dashboard container">
+            <Row>
+              <Col md={12}>
+                <h3 className="page-title">Resources</h3>
+              </Col>
+            </Row>
+            <Row>
+              <Statistics />
+            </Row>
+            <Row>
+              <ResourceList />
+            </Row>
+          </Container>
+        </div>
+      </div>
+    );
+  }
+
+}
 
 export default Resources;
