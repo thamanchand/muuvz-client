9880adac4635a498fdce2e43570680f30c4cfa74
Thaman <thaman.chand@invian.fi>
2019-05-11 20:56:53 +0300

    ResourcePage constants initi

---
+++ app/containers/Resources/constants.js
@@ -0,0 +1,9 @@
+/*
+ *
+ * ResourcePage constants
+ *
+ */
+
+export const ON_VANLIST_LOAD = 'app/ResourcePage/ON_VANLIST_LOAD';
+export const ON_VANLIST_LOAD_SUCCESS = 'app/ResourcePage/ON_VANLIST_LOAD_SUCCESS';
+export const ON_VANLIST_LOAD_FAILURE = 'app/ResourcePage/ON_VANLIST_LOAD_FAILURE';
