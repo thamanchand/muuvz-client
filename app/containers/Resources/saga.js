fce11cdeb00d1c3f4e432dc371a47ec5738f20a6
Thaman <thaman.chand@invian.fi>
2019-05-11 20:58:25 +0300

    ResourcePage saga init

---
+++ app/containers/Resources/saga.js
@@ -0,0 +1,32 @@
+import {
+  call,
+  takeLatest,
+  put,
+} from 'redux-saga/effects';
+
+// Utils
+import request from 'utils/request';
+
+// constants
+import {
+  onVanListLoadSuccess,
+  onVanListLoadFailure,
+} from './actions';
+
+import { ON_VANLIST_LOAD } from './constants';
+
+export function* vanLoadWatcher() {
+  try {
+    const requestURL = 'http://localhost:1337/vans';
+    const vanList = yield call(request, requestURL, { method: 'GET' });
+    if (vanList) {
+      yield put(onVanListLoadSuccess(vanList));
+    }
+  } catch(error) {
+    yield put(onVanListLoadFailure(error));
+  }
+}
+
+export default function* defaultSaga() {
+  yield takeLatest(ON_VANLIST_LOAD, vanLoadWatcher);
+}
