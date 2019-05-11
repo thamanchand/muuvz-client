b456eba84f34fa6e2f605fcfdf411bff30234df9
Thaman <thaman.chand@invian.fi>
2019-05-11 21:08:54 +0300

    dateFormat util

--- app/containers/utils.js
+++ app/containers/utils.js
@@ -1,3 +1,10 @@
+import moment from 'moment';
+
 export function formatNumber(num) {
   return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+}
+
+// date formatter 2019.01.23
+export function formatDate(date) {
+  return moment(date).format('YYYY-MM-DD')
 }
