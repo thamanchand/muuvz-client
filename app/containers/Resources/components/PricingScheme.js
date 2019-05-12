b42582d29c5579282687a5b4e2394da8fafa57c9
Thaman <thaman.chand@invian.fi>
2019-05-12 22:30:01 +0300

    PricingSchema component

---
+++ app/containers/Resources/components/PricingScheme.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Col } from 'reactstrap';
+
+const PricingSchema = () => (
+  <Col md={12} sm={12}>
+    <div className="form__form-group">
+      <span className="form__form-group-label">Price</span>
+    </div>
+  </Col>
+);
+
+export default PricingSchema;
