71aec26d3cd7671fbc5e99ac36b7538bdee99d8b
Thaman <thaman.chand@invian.fi>
2019-05-19 20:39:25 +0300

    Reusable ErrorField component

---
+++ app/shared/ErrorField/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Field } from 'react-final-form';
+
+const ErrorField = ({ name }) => (
+  <Field
+    name={name}
+    subscription={{ touched: true, error: true }}
+    render={({ meta: { touched, error } }) =>
+      touched && error
+        ?
+        <div className="validation__error">
+          {error}
+        </div>
+        :
+        null
+    }
+  />
+);
+
+ErrorField.propTypes = {
+  name: PropTypes.element.isRequired,
+};
+
+export default ErrorField;
