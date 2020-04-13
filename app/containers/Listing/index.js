10bce92c9ad953855b6a9f279388d55254e2b0ac
Thaman <thaman.chand@invian.fi>
2020-04-13 17:14:01 +0545

    handle onSearch action, isEdit state

--- app/containers/Listing/index.js
+++ app/containers/Listing/index.js
@@ -1,17 +1,16 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { bindActionCreators, compose } from 'redux';
 import { Container, Row, Col } from 'reactstrap';
 import { createStructuredSelector } from 'reselect';
+import { compose } from 'redux';
 
 // Utils
 import injectSaga from 'utils/injectSaga';
 import injectReducer from 'utils/injectReducer';
 
-import { onVanListLoad } from './actions';
-import { makeSelectVans } from './selectors';
-import Search from '../HomePage/components/Search';
+import { selectResourcesSelector } from './selectors';
+import Search from '../../shared/SearchForm';
 import VanListing from './components/VanListing';
 import saga from './saga';
 import reducer from './reducers';
@@ -19,25 +18,40 @@ import reducer from './reducers';
 const key = 'listingPage';
 
 class VanListPage extends PureComponent {
+  state = { isEdit: false }
 
-  componentDidMount() {
-    this.props.onVanListLoad();
+  onSearchEditToggle = () => {
+    const { isEdit } = this.state;
+    this.setState({ isEdit: !isEdit})
   }
 
   render() {
     const { vanList } = this.props;
+    const { isEdit } = this.state;
+
     return (
       <div className="Listing__page">
         <Container>
           <Row>
             <Col md={3} className="van__search">
               <div className="van__list-nav">
-                <Search />
+                <span
+                  role="button"
+                  tabIndex="0"
+                  onClick={this.onSearchEditToggle}
+                  onKeyDown={this.onSearchEditToggle}
+                >Edit</span>
+                <Search
+                  onSearch={this.onSearch}
+                  disabled={isEdit}
+                />
               </div>
             </Col>
             <Col md={9} className="van__content">
               <div className="van__list">
-                <VanListing vanList={vanList} />
+                <VanListing
+                  vanList={vanList}
+                />
               </div>
             </Col>
           </Row>
@@ -48,7 +62,7 @@ class VanListPage extends PureComponent {
 }
 
 VanListPage.propTypes = {
-  onVanListLoad: PropTypes.func,
+  // onVanListLoad: PropTypes.func,
   vanList: PropTypes.arrayOf(PropTypes.shape({
     _id: PropTypes.string,
     createdAt: PropTypes.string,
@@ -69,16 +83,13 @@ VanListPage.propTypes = {
 }
 
 const mapStateToProps = createStructuredSelector({
-  vanList: makeSelectVans(),
+  vanList: selectResourcesSelector(),
 });
 
-const mapDispatchToProps = (dispatch) => ({
-  onVanListLoad: bindActionCreators(onVanListLoad, dispatch)
-});
 
 const withConnect = connect(
   mapStateToProps,
-  mapDispatchToProps,
+  null,
 );
 
 const withReducer = injectReducer({ key, reducer });
