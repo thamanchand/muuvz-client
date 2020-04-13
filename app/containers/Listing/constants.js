3aba2c45fe8a7e9c921c135d42d184af2e2e2601
Thaman <thaman.chand@invian.fi>
2020-04-13 16:58:02 +0545

    ON_SEARCH, ON_SEARCH_SUCCESS, ON_SEARCH_FAILED init

---
+++ app/containers/Listing/constants.js
@@ -0,0 +1,3 @@
+export const ON_SEARCH = 'app/ResourcePage/ON_SEARCH';
+export const ON_SEARCH_SUCCESS = 'app/ResourcePage/ON_SEARCH_SUCCESS';
+export const ON_SEARCH_FAILED = 'app/ResourcePage/ON_SEARCH_FAILED';
--- app/containers/Listing/contants.js
+++
@@ -1,9 +0,0 @@
-/*
- *
- * VanListPage constants
- *
- */
-
-export const ON_VANLIST_LOAD = 'app/VanListPage/ON_VANLIST_LOAD';
-export const ON_VANLIST_LOAD_SUCCESS = 'app/VanListPage/ON_VANLIST_LOAD_SUCCESS';
-export const ON_VANLIST_LOAD_FAILURE = 'app/VanListPage/ON_VANLIST_LOAD_FAILURE';
