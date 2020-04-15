b4cdcf9dfc293ce1d6087bb030b2ce8e73486fe0
Thaman <thaman.chand@invian.fi>
2020-04-15 11:42:43 +0545

    searchResourceWatcher saga orchestration

--- app/containers/Listing/saga.js
+++ app/containers/Listing/saga.js
@@ -1,11 +1,43 @@
-// import {
-//   call,
-//   takeLatest,
-//   put,
-// } from 'redux-saga/effects';
-//
-// // Utils
-// import request from 'utils/request';
-// 
-// export default function* defaultSaga() {
-// }
+import {
+  call,
+  takeLatest,
+  put,
+} from 'redux-saga/effects';
+
+// Utils
+import request from 'utils/request';
+import  history from '../../utils/history';
+
+// constants
+import {
+  onSearchSuccess,
+  onSearchFailed,
+} from './actions';
+
+import {
+  ON_SEARCH,
+} from './constants';
+
+const baseURL = "http://localhost:1337/";
+
+export function* searchResourceWatcher(action) {
+  console.log("action", action);
+  try {
+    const requestURL = `${baseURL}${'resources'}`;
+    const searchResult = yield call(request, requestURL, { method: 'GET' });
+    if (searchResult) {
+      yield put(onSearchSuccess(searchResult));
+      yield call(forwardTo('/listing'))
+    }
+  } catch(error) {
+    yield put(onSearchFailed(error));
+  }
+}
+
+export default function* defaultSaga() {
+  yield takeLatest(ON_SEARCH, searchResourceWatcher);
+}
+
+function forwardTo(location) {
+  history.push(location);
+}
