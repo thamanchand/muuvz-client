bc25578df74248ad44c9ce369754cbf9cbce97fa
Thaman <thaman.chand@invian.fi>
2020-04-16 20:30:32 +0545

    ON_REGISTER_SUBMIT, ON_REGISTER_SUBMIT_SUCCESS, ON_REGISTER_SUBMIT_FAILED reducer states

--- app/containers/RegisterPage/actions.js
+++ app/containers/RegisterPage/actions.js
@@ -1,4 +1,8 @@
-import { ON_REGISTER_SUBMIT } from './constants';
+import { ON_REGISTER_SUBMIT,
+  ON_REGISTER_SUBMIT_SUCCESS,
+  ON_REGISTER_SUBMIT_FAILED,
+  ON_REGISTERPAGE_LOAD,
+} from './constants';
 /**
  * Sends the request to the API
  * @return {string}
@@ -7,4 +11,17 @@ import { ON_REGISTER_SUBMIT } from './constants';
 export const onRegisterSubmit = (payload) => ({
   type: ON_REGISTER_SUBMIT,
   payload,
+});
+
+export const onRegisterSuccess = () => ({
+  type: ON_REGISTER_SUBMIT_SUCCESS,
+});
+
+export const onRegisterFailed = (error) => ({
+  type: ON_REGISTER_SUBMIT_FAILED,
+  error
+});
+
+export const onRegisterPageLoad = () => ({
+  type: ON_REGISTERPAGE_LOAD,
 });
