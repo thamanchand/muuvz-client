c4e27f037f9b11a2f3d94e13b20e59d7e0504202
Thaman <thaman.chand@invian.fi>
2020-04-16 20:33:54 +0545

    ON_REGISTER_SUBMIT, ON_REGISTER_SUBMIT_SUCCESS, ON_REGISTERPAGE_LOAD reducer states

---
+++ app/containers/RegisterPage/reducer.js
@@ -0,0 +1,44 @@
+/*
+ *
+ * LoginPage reducer
+ *
+ */
+
+import { fromJS } from 'immutable';
+import {
+  ON_REGISTER_SUBMIT,
+  ON_REGISTER_SUBMIT_SUCCESS,
+  ON_REGISTER_SUBMIT_FAILED,
+  ON_REGISTERPAGE_LOAD,
+} from './constants';
+
+
+export const initialState = fromJS({
+  isEmailRegistered: false,
+});
+
+function registerPageReducer(state = initialState, action) {
+  switch (action.type) {
+    case ON_REGISTER_SUBMIT:
+      return state
+        .set('isEmailRegistered', false)
+        .set('error', null);
+      ;
+
+    case ON_REGISTER_SUBMIT_SUCCESS:
+      return state.set('isEmailRegistered', true);
+
+    case ON_REGISTER_SUBMIT_FAILED:
+      return state
+        .set('error', fromJS(action.error))
+        .set('isEmailRegistered', false);
+
+    case ON_REGISTERPAGE_LOAD:
+      return state.set('isEmailRegistered', false);
+
+    default:
+      return state;
+  }
+}
+
+export default registerPageReducer;
