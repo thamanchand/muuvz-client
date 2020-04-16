31398b8da23b77adb3e33be82a09330a71d600c8
Thaman <thaman.chand@invian.fi>
2020-04-16 20:31:21 +0545

    ON_REGISTER_SUBMIT_SUCCESS, ON_REGISTER_SUBMIT_FAILED, ON_REGISTERPAGE_LOAD constants

--- app/containers/RegisterPage/constants.js
+++ app/containers/RegisterPage/constants.js
@@ -5,3 +5,7 @@
  */
 
 export const ON_REGISTER_SUBMIT = 'app/RegisterPage/ON_REGISTER_SUBMIT';
+export const ON_REGISTER_SUBMIT_SUCCESS = 'app/RegisterPage/ON_REGISTER_SUBMIT_SUCCESS';
+export const ON_REGISTER_SUBMIT_FAILED = 'app/RegisterPage/ON_REGISTER_SUBMIT_FAILED';
+
+export const ON_REGISTERPAGE_LOAD = 'app/RegisterPage/ON_REGISTERPAGE_LOAD';
