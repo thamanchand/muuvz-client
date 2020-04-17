22e02e4f839a8547d4d527e7984c59f01d9f7f81
Thaman <thaman.chand@invian.fi>
2020-04-17 18:32:28 +0545

    display avatar email

--- app/containers/Layout/topbar/TopbarProfile.js
+++ app/containers/Layout/topbar/TopbarProfile.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import avatar from '../../../assets/images/topbar/ava.png';
 
-const TopbarProfile = () => (
-  <div className="topbar__profile">
-    <button type="button" className="topbar__avatar">
-      <img className="topbar__avatar-img" src={avatar} alt="avatar" />
-      <p className="topbar__avatar-name">Roman Johanson</p>
-    </button>
-  </div>
-);
+import auth from '../../../utils/auth';
+
+const TopbarProfile = () => {
+  const userEmail = auth.get('userInfo') && auth.get('userInfo').email;
+  return (
+    <div className="topbar__profile">
+      <button type="button" className="topbar__avatar">
+        <img className="topbar__avatar-img" src={avatar} alt="avatar" />
+        <p className="topbar__avatar-name">{userEmail}</p>
+      </button>
+    </div>
+  )
+}
 
 export default TopbarProfile;
