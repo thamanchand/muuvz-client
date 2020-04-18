e679e33fbe08a2a8a3364d759030ce9415e6b682
Thaman <thaman.chand@invian.fi>
2020-04-18 19:34:33 +0545

    added new profile constants

---
+++ app/containers/Profile/constants.js
@@ -0,0 +1,11 @@
+export const ON_PROFILEPAGE_LOAD = 'app/ResourcePage/ON_PROFILEPAGE_LOAD';
+export const ON_PROFILEPAGE_LOAD_SUCCESS = 'app/ResourcePage/ON_PROFILEPAGE_LOAD_SUCCESS';
+export const ON_PROFILEPAGE_LOAD_FAILED = 'app/ResourcePage/ON_PROFILEPAGE_LOAD_FAILED';
+
+export const ON_PROFILEPAGE_SAVE = 'app/ResourcePage/ON_PROFILEPAGE_SAVE';
+export const ON_PROFILEPAGE_SAVE_SUCCESS = 'app/ResourcePage/ON_PROFILEPAGE_SAVE_SUCCESS';
+export const ON_PROFILEPAGE_SAVE_FAILED = 'app/ResourcePage/ON_PROFILEPAGE_SAVE_FAILED';
+
+export const ON_PROFILE_EDIT = 'app/ResourcePage/ON_PROFILE_EDIT';
+export const ON_PROFILE_EDIT_SUCCESS = 'app/ResourcePage/ON_PROFILE_EDIT_SUCCESS';
+export const ON_PROFILE_EDIT_FAILED = 'app/ResourcePage/ON_PROFILE_EDIT_FAILED';
