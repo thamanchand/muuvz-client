404a138b64c23bdf7a39c7933e8d16708b8ce630
Thaman <thaman.chand@invian.fi>
2020-04-18 19:34:58 +0545

    profile actions init

---
+++ app/containers/Profile/action.js
@@ -0,0 +1,61 @@
+import {
+  ON_PROFILEPAGE_LOAD,
+  ON_PROFILEPAGE_LOAD_SUCCESS,
+  ON_PROFILEPAGE_LOAD_FAILED,
+  ON_PROFILEPAGE_SAVE,
+  ON_PROFILEPAGE_SAVE_SUCCESS,
+  ON_PROFILEPAGE_SAVE_FAILED,
+  ON_PROFILE_EDIT,
+  ON_PROFILE_EDIT_SUCCESS,
+  ON_PROFILE_EDIT_FAILED,
+} from './constants';
+/**
+ * Sends the request to the API
+ * @return {string}
+ */
+
+export const onProfileSave = (profilePayload) => ({
+  type: ON_PROFILEPAGE_SAVE,
+  profilePayload,
+});
+
+export const onProfileSaveSuccess = (userProfile) => ({
+  type: ON_PROFILEPAGE_SAVE_SUCCESS,
+  userProfile,
+});
+
+export const onProfileSaveFailed = (error) => ({
+  type: ON_PROFILEPAGE_SAVE_FAILED,
+  error
+});
+
+export const onProfileLoad = (userId) => ({
+  type: ON_PROFILEPAGE_LOAD,
+  userId,
+});
+
+export const onProfileLoadSuccess = (userProfile) => ({
+  type: ON_PROFILEPAGE_LOAD_SUCCESS,
+  userProfile,
+});
+
+export const onProfileLoadFailed = (error) => ({
+  type: ON_PROFILEPAGE_LOAD_FAILED,
+  error
+});
+
+export const onProfileEdit = (profileId, profilePayload) => ({
+  type: ON_PROFILE_EDIT,
+  profileId,
+  profilePayload,
+});
+
+export const onProfileEditSuccess = (userProfile) => ({
+  type: ON_PROFILE_EDIT_SUCCESS,
+  userProfile,
+});
+
+export const onProfileEditFailed = (error) => ({
+  type: ON_PROFILE_EDIT_FAILED,
+  error
+});
