8623d96d5c98bf94a709cf2a44b50b388e39cbec
Thaman <thaman.chand@invian.fi>
2020-04-19 10:12:19 +0545

    Complete AccountForm with password & confirmPassword

--- app/containers/AccountPage/components/AccountForm.js
+++ app/containers/AccountPage/components/AccountForm.js
@@ -1,76 +1,93 @@
 import React from 'react';
-import { Card, CardBody, Col, Button, ButtonToolbar } from 'reactstrap';
+import PropTypes from 'prop-types';
+import { Card, CardBody, Col } from 'reactstrap';
 import { Form, Field } from 'react-final-form';
 
-const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+import Error from '../../../shared/ErrorField';
 
-const onSubmit = async values => {
-  await sleep(300);
-  window.alert(JSON.stringify(values, 0, 2));
-};
+import auth from '../../../utils/auth';
+const userEmail = auth.get('userInfo') && auth.get('userInfo').email;
 
-const AccountForm = () => (
+const AccountForm = ({ changePassword }) => (
   <Col md={12} lg={12}>
     <Card>
       <CardBody>
         <div className="card__title">
           <h5 className="bold-text">Settings</h5>
-          <h5 className="subhead">Fill all fields</h5>
         </div>
         <Form
-          onSubmit={onSubmit}
-          initialValues={{ bname: 'xyz oy', bId: '1334565-8' }}
+          onSubmit={changePassword}
           render={({ handleSubmit }) => (
-            <form className="form form--vertical" onSubmit={handleSubmit}>
-              <div className="form__half">
-                <div className="form__form-group">
-                  <span className="form__form-group-label">Email</span>
-                  <div className="form__form-group-field">
-                    <Field
-                      name="email"
-                      component="input"
-                      type="email"
-                      placeholder="abc@pakettioy.fi"
-                    />
-                  </div>
-                </div>
-                <div className="form__form-group">
-                  <span className="form__form-group-label">Phone</span>
-                  <div className="form__form-group-field">
-                    <Field
-                      name="phonenum"
-                      component="input"
-                      type="text"
-                      placeholder="03737373"
-                    />
-                  </div>
+
+            <form className="form form-small form--horizontal" onSubmit={handleSubmit}>
+              <div className="form__form-group">
+                <span className="form__form-group-label">Username</span>
+                <div className="form__form-group-field">
+                  <Field
+                    name="email"
+                    component="input"
+                    type="text"
+                    placeholder={userEmail}
+                    disabled
+                  />
                 </div>
               </div>
+            </form>
+          )}
+        />
+        <div className="card__title">
+          <h5 className="bold-text">Password</h5>
+        </div>
+        <Form
+          validate={values => { // validate both passowrds are same
+            const errors = {};
+            if (values.password !== values.confirmPassword) {
+              errors.confirmPassword = 'Password didnt matched';
+            }
+            if (!values.password) {
+              errors.password = 'password cant be blank';
+            }
+            if (!values.confirmPassword) {
+              errors.confirmPassword = 'confirm password cant be blank';
+            }
+            return errors
+          }}
+          onSubmit={changePassword}
+          render={({ handleSubmit }) => (
 
-              <div className="form__half">
-                <div className="form__form-group">
-                  <span className="form__form-group-label">Password</span>
-                  <div className="form__form-group-field">
-                    <Field name="password" component="input" type="password" />
-                  </div>
+            <form className="form form-small form--horizontal" onSubmit={handleSubmit}>
+              <div className="form__form-group">
+                <span className="form__form-group-label">Password</span>
+                <div className="form__form-group-field">
+                  <Field
+                    name="password"
+                    component="input"
+                    type='password'
+                    placeholder="Password"
+                  />
                 </div>
+                <Error name="password" />
+              </div>
 
-                <div className="form__form-group">
-                  <span className="form__form-group-label">Repassword</span>
-                  <div className="form__form-group-field">
-                    <Field
-                      name="repassword"
-                      component="input"
-                      type="password"
-                    />
-                  </div>
+              <div className="form__form-group">
+                <span className="form__form-group-label">Re-Password</span>
+                <div className="form__form-group-field">
+                  <Field
+                    name="confirmPassword"
+                    component="input"
+                    type='password'
+                    placeholder="Re-Password"
+                  />
+
                 </div>
+                <Error name="confirmPassword" />
+              </div>
+              <div className="profile__btns">
+                <button
+                  className="square btn btn-primary"
+                  type="submit"
 
-                <ButtonToolbar className="form__button-toolbar">
-                  <Button className="square btn btn-primary" type="submit">
-                    Submit
-                  </Button>
-                </ButtonToolbar>
+                >Chanege password</button>
               </div>
             </form>
           )}
@@ -80,4 +97,7 @@ const AccountForm = () => (
   </Col>
 );
 
+AccountForm.propTypes = {
+  changePassword: PropTypes.func
+}
 export default AccountForm;
