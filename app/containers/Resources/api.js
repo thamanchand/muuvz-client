7aa202080e22e0b3b9338956635a9a65430426ae
Thaman <thaman.chand@invian.fi>
2020-04-20 10:54:57 +0545

    added resource api endpoints using axios

--- app/containers/Resources/api.js
+++ app/containers/Resources/api.js
@@ -1,7 +1,32 @@
 import axios from 'axios';
 
-// post van
-export const postResource = (body) => axios.post('http://localhost:1337/resource', body).then(res => {
-  console.log(res);
-  return res.data.user.id;
-})
+import auth from '../../utils/auth';
+
+const token = auth.getToken();
+
+// post resource
+export const postResources = data => axios
+  .post(
+    'http://localhost:1337/resource',
+
+    { data,
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    },
+
+  ).then(response => response.data)
+  .catch(error => {
+    throw error;
+  });
+
+
+// data api calls
+export const getResources = () => axios.get('http://localhost:1337/resources', {
+  headers: {
+    'Accept': 'application/json',
+    'Authorization': `Bearer ${token}`,
+  }
+}).then(response => response.data).catch(err => {
+  throw err;
+});
