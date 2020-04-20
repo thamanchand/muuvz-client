e4f1a13eed2d3753bb1b37236fce6748db19bbe1
Thaman <thaman.chand@invian.fi>
2020-04-20 12:49:05 +0545

    fix postResources and postPricing utils payload and add Authorization header

--- app/containers/Resources/api.js
+++ app/containers/Resources/api.js
@@ -5,21 +5,32 @@ import auth from '../../utils/auth';
 const token = auth.getToken();
 
 // post resource
-export const postResources = data => axios
+export const postResources = body => axios
   .post(
-    'http://localhost:1337/resource',
-
-    { data,
-      headers: {
-        'Authorization': `Bearer ${token}`
-      }
-    },
-
+    'http://localhost:1337/resources',
+    { ...body },
+    {headers: {
+      'Authorization': `Bearer ${token}`
+    }},
   ).then(response => response.data)
   .catch(error => {
     throw error;
   });
 
+// post pricing
+export const postPricing = payload => axios
+  .post(
+    'http://localhost:1337/pricings',
+    { ...payload },
+    {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    },
+  ).then(response => response.data)
+  .catch(error => {
+    throw error;
+  });
 
 // data api calls
 export const getResources = () => axios.get('http://localhost:1337/resources', {
