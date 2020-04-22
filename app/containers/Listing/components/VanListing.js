75be897568e54f37d746361e63f0e1e9634ed782
Thaman <thaman.chand@invian.fi>
2020-04-22 17:15:41 +0545

    map price and images

--- app/containers/Listing/components/VanListing.js
+++ app/containers/Listing/components/VanListing.js
@@ -6,10 +6,9 @@ import {heartO} from 'react-icons-kit/fa/heartO'
 import {ic_euro_symbol as euroSymbol} from 'react-icons-kit/md/ic_euro_symbol'
 
 // import { formatNumber } from '../../utils';
+import GalleySlideshow from '../../../shared/GalleySlideshow';
 
 const { uuid } = require('uuidv4');
-
-// import VanGallery from './VanGallery';
 const iconStyles = {
   fontFamily: 'flaticon',
   fontStyle: 'normal',
@@ -23,41 +22,46 @@ const VanListing = ({ vanList }) => (
         <section className="vanlist">
           <div className="van__content">
             {vanList && vanList.map(item => (
-              <div className="media" key={uuid()}>
-                <div className="fav-box">
-                  <Icon icon={heartO} size={15} />
+              <div>
+                <div className="media" key={uuid()}>
+                  <div className="fav-box">
+                    <Icon icon={heartO} size={15} />
 
-                </div>
-                <div className="pricing__content">
-                  <span className="price__icon"><Icon icon={euroSymbol} size={25} /></span>
-                  {item.pricings.map((cost) => (
-                    <span className="price-large" key={uuid()}>
-                      {cost.price}
-                    </span>
-                  ))}
-                  <span className="hr-small">,hr</span>
-                  <button type="button" className="square btn btn-danger">Book</button>
-                </div>
-                {/* item.vanmedias.map(img => (
-                    <img className="d-flex align-self-start" src={img.filename[0.].name} alt={item.brand} />
-                  )) */}
-                <div className="media-body pl-3">
-                  <div className="price">{item.brand}<small>{item.model}</small></div>
-                  <div className="stats">
-                    <span>
-                      <i className="flaticon-049-gasoline" style={iconStyles}></i>
-                      {item.fueltype}
-                    </span>
-                    <span>
-                      <i className="flaticon-015-gear" style={iconStyles}></i>
-                      {item.transmission}
-                    </span>
-                    <span>
-                      <i className="flaticon-037-wristwatch" style={iconStyles}></i>
-                      {item.mileage}
-                    </span>
                   </div>
-                  <div className="address">{item.features}</div>
+                  <div className="pricing__content">
+                    <span className="price__icon"><Icon icon={euroSymbol} size={25} /></span>
+                    {item.pricing && item.pricing.map((cost) => (
+                      <span className="price-large" key={uuid()}>
+                        {cost.price}
+                      </span>
+                    ))}
+                    <span className="hr-small">,hr</span>
+                    <button type="button" className="square btn btn-danger">Book</button>
+                  </div>
+                  <div className="slideshow">
+                    <GalleySlideshow input={item.cover} ratio="3:2" mode="manual" />
+                  </div>
+                  {/* item.vanmedias.map(img => (
+                      <img className="d-flex align-self-start" src={img.filename[0.].name} alt={item.brand} />
+                    )) */}
+                  <div className="media-body pl-3">
+                    <div className="price">{item.brand}<small>{item.model}</small></div>
+                    <div className="stats">
+                      <span>
+                        <i className="flaticon-049-gasoline" style={iconStyles}></i>
+                        {item.fueltype}
+                      </span>
+                      <span>
+                        <i className="flaticon-015-gear" style={iconStyles}></i>
+                        {item.transmission}
+                      </span>
+                      <span>
+                        <i className="flaticon-037-wristwatch" style={iconStyles}></i>
+                        {item.mileage}
+                      </span>
+                    </div>
+                    <div className="address">{item.features}</div>
+                  </div>
                 </div>
               </div>
             ))}
