6bdadd7e5193873296116813d710c9d2779da1d3
Thaman <thaman.chand@invian.fi>
2020-04-22 13:49:57 +0545

    fix profile save, edit, load

--- app/containers/Profile/saga.js
+++ app/containers/Profile/saga.js
@@ -2,14 +2,12 @@ import {
   call,
   takeLatest,
   put,
+  select,
 } from 'redux-saga/effects';
 
-// Utils
-import request from 'utils/request';
-
-import axios from 'axios';
 
 import auth from '../../utils/auth';
+import { userProfileSelector } from './selector';
 
 // constants
 import {
@@ -27,45 +25,56 @@ import {
   ON_PROFILE_EDIT,
 } from './constants';
 
-const baseURL = "http://localhost:1337/";
+import * as api from './api';
 
 export function* profileSaveWatcher(action) {
   console.log("Save action", action)
 
   const userId = auth.get('userInfo') && auth.get('userInfo').id;
-  let body = {...action.profilePayload, user: userId};
+  const userProfilePayload = {...action.profilePayload, user: userId};
   try {
-    const requestURL = `${baseURL}${'profiles'}`;
-    // const userProfile = yield call(request, requestURL, { method: 'GET' });
-    const response = yield call(request, requestURL, { method: 'POST', body });
-    console.log("Save response", response)
-    if (response
-      && action.profilePayload.businessName
-      && action.profilePayload.phoneNumber
-      && action.profilePayload.address) {
-      body = {profileCompleted: true };
-      // update user profileCompleted: true once profile is created
-      const updateUserProfile = `${baseURL}${'users'}/${userId}`;
-      // const userFieldsLocallyStored = { email, confirmed, profileCompleted, isbusiness}
+    const profilePostResponse = yield call(api.postUserProfile,  userProfilePayload);
 
-      const updateResponse = yield call(request, updateUserProfile, { method: 'put', body });
-      const { email, confirmed, profileCompleted, isbusiness, id } = updateResponse;
+    if (profilePostResponse) {
+      const profileCompletePayload = {profileCompleted: true };
+
+      // update user profileCompleted: true once profile is created
+      const profileUpdateResponse = yield call(api.updateUserProfile, userId, profileCompletePayload);
+
+      const { email, confirmed, profileCompleted, isbusiness, id } = profileUpdateResponse;
       const userFieldsLocallyStored = { email, confirmed, profileCompleted, isbusiness, id}
+
+      // upload profile image
+      // prepare upload formData
+      const data = new FormData();
+      data.append('files', action.profilePayload.files[0]);
+      data.append('refId', profilePostResponse.id);
+      data.append('ref', 'profile');
+      data.append('field', 'avatar');
+
+      // upload profile avatar
+      const avatarUploadResponse = yield call(api.uploadProfileAvatar, data);
+      // call get profile data
+      const profileApiResponse = yield call(api.getUserProfile, profilePostResponse.id);
+
       const userProfile = {
-        id: response.id,
-        businessName: response.businessName,
-        businessId: response.businessId,
-        website: response.website,
-        description: response.description,
-        phoneNumber: response.phoneNumber,
-        facebookLink: response.facebookLink,
-        twitterLink: response.twitterLink,
-        linkedinLink: response.linkedinLink,
-        address: response.address,
+        id: profileApiResponse.id,
+        businessName: profilePostResponse.businessName,
+        businessId: profileApiResponse.businessId,
+        website: profileApiResponse.website,
+        description: profileApiResponse.description,
+        phoneNumber: profileApiResponse.phoneNumber,
+        facebookLink: profileApiResponse.facebookLink,
+        twitterLink: profileApiResponse.twitterLink,
+        linkedinLink: profileApiResponse.linkedinLink,
+        address: profileApiResponse.address,
+        avatar: profileApiResponse.avatar,
       }
-      // update userInfo cookie
-      yield call(auth.setUserInfo, userFieldsLocallyStored);
-      yield put(onProfileSaveSuccess(userProfile));
+      if (avatarUploadResponse && profileUpdateResponse ) {
+        // update userInfo cookie
+        yield call(auth.setUserInfo, userFieldsLocallyStored);
+        yield put(onProfileSaveSuccess(userProfile));
+      }
     }
   } catch(error) {
     yield put(onProfileSaveFailed(error));
@@ -73,30 +82,28 @@ export function* profileSaveWatcher(action) {
 }
 
 export function* profileLoadWatcher(action) {
-  console.log("Load action", action)
+  console.log("Load prrofile action", action);
+
   try {
-    const requestURL = `${baseURL}${'users'}/${action.userId}`;
-    const userAPIresponse = yield call(request, requestURL, { method: 'GET' });
-    console.log("userAPIresponse", userAPIresponse)
+    const userAPIresponse = yield call(api.getUserInfo, action.userId);
     const { id } = userAPIresponse.profile;
     if (userAPIresponse) {
 
-      const requestProfileURL = `${baseURL}${'profiles'}/${id}`;
-      const response = yield call(request, requestProfileURL, { method: 'GET' });
+      const profileApiResponse = yield call(api.getUserProfile, id);
       const userProfile = {
-        id: response.id,
-        businessName: response.businessName,
-        businessId: response.businessId,
-        website: response.website,
-        description: response.description,
-        phoneNumber: response.phoneNumber,
-        facebookLink: response.facebookLink,
-        twitterLink: response.twitterLink,
-        linkedinLink: response.linkedinLink,
-        address: response.address
+        id: profileApiResponse.id,
+        businessName: profileApiResponse.businessName,
+        businessId: profileApiResponse.businessId,
+        website: profileApiResponse.website,
+        description: profileApiResponse.description,
+        phoneNumber: profileApiResponse.phoneNumber,
+        facebookLink: profileApiResponse.facebookLink,
+        twitterLink: profileApiResponse.twitterLink,
+        linkedinLink: profileApiResponse.linkedinLink,
+        address: profileApiResponse.address,
+        avatar: profileApiResponse.avatar,
       }
-      console.log("userProfile", userProfile);
-      if (response) {
+      if (userAPIresponse && profileApiResponse) {
         yield put(onProfileLoadSuccess(userProfile));
       }
     }
@@ -106,64 +113,58 @@ export function* profileLoadWatcher(action) {
 }
 
 export function* profileEditWatcher(action) {
-  console.log("edit watcher", action);
-  const body = {...action.profilePayload};
+  console.log("EDIT PRROFILE", action);
+
+  const editPayload = {...action.profilePayload};
+
+  const getUserProfile = yield select(userProfileSelector());
+  const isFileUploaded = action.profilePayload.files[0];
+  const avatarId = getUserProfile.avatar && getUserProfile.avatar.id;
   try {
-    const requestURL = `${baseURL}${'profiles'}/${action.profileId}`;
-    const response = yield call(request, requestURL, { method: 'put', body });
-    console.log("Edit response", response);
-    if (response) {
-      const userProfile = {
-        id: response.id,
-        businessName: response.businessName,
-        businessId: response.businessId,
-        website: response.website,
-        description: response.description,
-        phoneNumber: response.phoneNumber,
-        facebookLink: response.facebookLink,
-        twitterLink: response.twitterLink,
-        linkedinLink: response.linkedinLink,
-        address: response.address,
+    const editProfileResponse = yield call(api.editUserProfile,  action.profileId, editPayload);
+    if (editProfileResponse) {
+      let userProfile = {
+        id: editProfileResponse.id,
+        businessName: editProfileResponse.businessName,
+        businessId: editProfileResponse.businessId,
+        website: editProfileResponse.website,
+        description: editProfileResponse.description,
+        phoneNumber: editProfileResponse.phoneNumber,
+        facebookLink: editProfileResponse.facebookLink,
+        twitterLink: editProfileResponse.twitterLink,
+        linkedinLink: editProfileResponse.linkedinLink,
+        address: editProfileResponse.address,
+        avatar: editProfileResponse.avatar,
       }
-      const isFileUploaded = action.profilePayload.files[0];
-      if (isFileUploaded instanceof File && !!response.avatar.id) {
-        const deleteResponse = yield axios({
-          method: 'DELETE',
-          url: `${baseURL}${'upload/files'}/${response.avatar.id}`
-        });
+
+      if (isFileUploaded instanceof File && avatarId) {
+        const deleteResponse = yield call(api.deleteProfileImage, editProfileResponse.avatar.id);
+
         if (deleteResponse) {
-          console.log("deleteResponse", deleteResponse)
-          const imageUploadURL = `${baseURL}${'upload'}`;
           const data = new FormData();
           data.append('files', action.profilePayload.files[0]);
-          data.append('refId', action.profileId);
+          data.append('refId', editProfileResponse.id);
           data.append('ref', 'profile');
           data.append('field', 'avatar');
-          const avatarUploadResponse = yield axios({
-            method: 'POST',
-            url: imageUploadURL,
-            data
-          })
-          console.log("avatarUploadResponse", avatarUploadResponse);
-          yield put(onProfileEditSuccess(userProfile));
+
+          const avatarUploadResponse = yield call(api.uploadProfileAvatar, data);
+          if (avatarUploadResponse && deleteResponse) {
+            userProfile = { ...userProfile, avatar: avatarUploadResponse[0]}
+            yield put(onProfileEditSuccess(userProfile));
+          }
         }
       } else {
-        const imageUploadURL = `${baseURL}${'upload'}`;
         const data = new FormData();
         data.append('files', action.profilePayload.files[0]);
-        data.append('refId', action.profileId);
+        data.append('refId', editProfileResponse.id);
         data.append('ref', 'profile');
         data.append('field', 'avatar');
-        const avatarUploadResponse = yield axios({
-          method: 'POST',
-          url: imageUploadURL,
-          data
-        })
-
-        console.log("avatarUploadResponse", avatarUploadResponse);
-        yield put(onProfileEditSuccess(userProfile));
+        const avatarUploadResponse = yield call(api.uploadProfileAvatar, data);
+        if (avatarUploadResponse) {
+          yield put(onProfileEditSuccess(userProfile));
+        }
       }
-
+      yield put(onProfileEditSuccess(userProfile));
     }
   } catch(error) {
     yield put(onProfileEditFailed(error));
