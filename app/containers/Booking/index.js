e2bf1a004f7ce7bc825c21c5a39b05a96b25a2df
Thaman <thaman.chand@invian.fi>
2020-04-24 12:23:27 +0545

    convert functional component to class, dispatch onBookingLoad action, mapStateToProps, connect

--- app/containers/Booking/index.js
+++ app/containers/Booking/index.js
@@ -1,41 +1,94 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Col, Container, Row } from 'reactstrap';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { bindActionCreators, compose } from 'redux';
+
+// Utils
+import injectSaga from 'utils/injectSaga';
+import injectReducer from 'utils/injectReducer';
+import auth from '../../utils/auth';
+
+import { onBookingLoad } from './action';
+import { selectBookingSelector } from './selector';
+
+import saga from './saga';
+import reducer from './reducer';
 
 import Layout from '../Layout/index';
 import Statistics from './components/Statistics';
 import BigCalendar from './components/BigCalendar';
 import EventLabels from './components/VanLabels';
 
-import auth from '../../utils/auth';
+const key = 'bookingPage';
 
 const isProfileCompleted = auth.get('userInfo') && auth.get('userInfo').profileCompleted;
 
-const BookingDashboard = () => (
-  <div>
-    <Layout />
-    <div className="container__wrap">
-      <Container className="dashboard container">
-        <Row>
-          <Col md={12}>
-            <h3 className="page-title">Booking</h3>
-          </Col>
-        </Row>
-        {isProfileCompleted && (
-          <Row>
-            <Statistics />
-          </Row>
-        )}
-        <Row>
-          <div className="container dashboard">
+
+class BookingDashboard extends React.PureComponent {
+
+  componentDidMount() {
+    this.props.onBookingLoad();
+  }
+
+  render() {
+    const { bookingList } = this.props;
+
+    return (
+      <div>
+        <Layout />
+        <div className="container__wrap">
+          <Container className="dashboard container">
             <Row>
-              <BigCalendar />
-              <EventLabels />
+              <Col md={12}>
+                <h3 className="page-title">Booking</h3>
+              </Col>
             </Row>
-          </div>
-        </Row>
-      </Container>
-    </div>
-  </div>
+            {isProfileCompleted && (
+              <Row>
+                <Statistics />
+              </Row>
+            )}
+            <Row>
+              <div className="container dashboard">
+                <Row>
+                  <BigCalendar bookingList={bookingList} />
+                  <EventLabels bookingList={bookingList} />
+                </Row>
+              </div>
+            </Row>
+          </Container>
+        </div>
+      </div>
+    )
+  }
+};
+
+BookingDashboard.propTypes = {
+  onBookingLoad: PropTypes.func,
+  bookingList: PropTypes.array,
+}
+const mapStateToProps = createStructuredSelector({
+  bookingList: selectBookingSelector(),
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  onBookingLoad: bindActionCreators(onBookingLoad, dispatch),
+});
+
+
+
+const withConnect = connect(
+  mapStateToProps,
+  mapDispatchToProps,
 );
 
-export default BookingDashboard;
+const withReducer = injectReducer({ key, reducer });
+const withSaga = injectSaga({ key, saga });
+
+export default compose(
+  withReducer,
+  withSaga,
+  withConnect,
+)(BookingDashboard);
