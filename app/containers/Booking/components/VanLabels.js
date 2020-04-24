5f71be5e4e56902f284bdf41550454343b387410
Thaman <thaman.chand@invian.fi>
2020-04-24 12:34:30 +0545

    map event name and color

--- app/containers/Booking/components/VanLabels.js
+++ app/containers/Booking/components/VanLabels.js
@@ -1,26 +1,35 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import { Card, CardBody, Col } from 'reactstrap';
 
-const EventLabels = () => (
+const EventLabels = ({ bookingList }) => (
   <Col md={12} lg={12} xl={3}>
     <Card className="card--not-full-height">
       <CardBody>
         <div className="card__title">
-          <h5 className="bold-text">Van labels</h5>
+          <h5 className="bold-text">Resource labels</h5>
         </div>
-        <p>
-          <span className="calendar-label calendar-label--red" /> Renault V3
-        </p>
-        <p>
-          <span className="calendar-label calendar-label--green" /> BMW highland
-          events
-        </p>
-        <p>
-          <span className="calendar-label calendar-label--blue" /> Scandia
-        </p>
+        {bookingList.map(event => (
+          <p>
+            <span className="calendar-label" style={{backgroundColor: event.resource.color}} /> {event.resource.brand}
+          </p>
+        ))}
+
       </CardBody>
     </Card>
   </Col>
 );
+
+EventLabels.propTypes = {
+  bookingList: PropTypes.arrayOf(PropTypes.shape({
+    event: PropTypes.id,
+    title: PropTypes.string,
+    start: PropTypes.obj,
+    end: PropTypes.obj,
+    color: PropTypes.string
+  }).isRequired
+  )
+};
 
 export default EventLabels;
