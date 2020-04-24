cd0eef7e3a69497f4289f0fd8853ea7b691be047
Thaman <thaman.chand@invian.fi>
2020-04-24 12:33:20 +0545

    map booking events

--- app/containers/Booking/components/BigCalendar.js
+++ app/containers/Booking/components/BigCalendar.js
@@ -1,16 +1,40 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Card, CardBody, Col } from 'reactstrap';
 import Calendar from './Calendar';
-import events from './events';
 
-const BigCalendar = () => (
-  <Col md={12} lg={12} xl={9}>
-    <Card>
-      <CardBody>
-        <Calendar events={events} />
-      </CardBody>
-    </Card>
-  </Col>
-);
+const BigCalendar = ({ bookingList }) => {
+  const events = bookingList.map(event => {
+    const mapEvents = {
+      id: event.id,
+      title: event.resource.brand,
+      start: new Date(event.bookedStartDateTime),
+      end: new Date(event.bookedEndDateTime),
+      color: event.resource.color,
+    }
+    return mapEvents;
+  })
+
+  return (
+    <Col md={12} lg={12} xl={9}>
+      <Card>
+        <CardBody>
+          <Calendar events={events} />
+        </CardBody>
+      </Card>
+    </Col>
+  )
+};
+
+BigCalendar.propTypes = {
+  bookingList: PropTypes.arrayOf(PropTypes.shape({
+    event: PropTypes.id,
+    title: PropTypes.string,
+    start: PropTypes.obj,
+    end: PropTypes.obj,
+    color: PropTypes.string
+  }).isRequired
+  )
+};
 
 export default BigCalendar;
