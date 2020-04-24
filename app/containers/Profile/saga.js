5d8cf18602853b94736cf1fec8d940778e468862
Thaman <thaman.chand@invian.fi>
2020-04-24 11:10:55 +0545

    WIP avatarDeleteWatcher, profileEditWatcher, profileLoadWatcher

--- app/containers/Profile/saga.js
+++ app/containers/Profile/saga.js
@@ -2,12 +2,11 @@ import {
   call,
   takeLatest,
   put,
-  select,
 } from 'redux-saga/effects';
-
+import { push } from 'react-router-redux';
 
 import auth from '../../utils/auth';
-import { userProfileSelector } from './selector';
+// import { userProfileSelector } from './selector';
 
 // constants
 import {
@@ -16,13 +15,16 @@ import {
   onProfileLoadSuccess,
   onProfileLoadFailed,
   onProfileEditSuccess,
-  onProfileEditFailed,
+  // onProfileEditFailed,
+  onUserProfileDeleteSuccess,
+  onUserProfileDeleteFailed,
 } from './action';
 
 import {
   ON_PROFILEPAGE_SAVE,
   ON_PROFILEPAGE_LOAD,
   ON_PROFILE_EDIT,
+  ON_USERPROFILE_DELETE,
 } from './constants';
 
 import * as api from './api';
@@ -43,8 +45,6 @@ export function* profileSaveWatcher(action) {
 
       const { email, confirmed, profileCompleted, isbusiness, id } = profileUpdateResponse;
       const userFieldsLocallyStored = { email, confirmed, profileCompleted, isbusiness, id}
-
-      // upload profile image
       // prepare upload formData
       const data = new FormData();
       data.append('files', action.profilePayload.files[0]);
@@ -116,58 +116,117 @@ export function* profileEditWatcher(action) {
   console.log("EDIT PRROFILE", action);
 
   const editPayload = {...action.profilePayload};
+  const userId = auth.get('userInfo') && auth.get('userInfo').id;
+  const isFileUploaded = action.profilePayload && action.profilePayload.files;
 
-  const getUserProfile = yield select(userProfileSelector());
-  const isFileUploaded = action.profilePayload.files[0];
-  const avatarId = getUserProfile.avatar && getUserProfile.avatar.id;
+  const profileCompletePayload = {profileCompleted: true };
+
   try {
-    const editProfileResponse = yield call(api.editUserProfile,  action.profileId, editPayload);
-    if (editProfileResponse) {
-      let userProfile = {
-        id: editProfileResponse.id,
-        businessName: editProfileResponse.businessName,
-        businessId: editProfileResponse.businessId,
-        website: editProfileResponse.website,
-        description: editProfileResponse.description,
-        phoneNumber: editProfileResponse.phoneNumber,
-        facebookLink: editProfileResponse.facebookLink,
-        twitterLink: editProfileResponse.twitterLink,
-        linkedinLink: editProfileResponse.linkedinLink,
-        address: editProfileResponse.address,
-        avatar: editProfileResponse.avatar,
-      }
+    if (isFileUploaded) {
+      console.log("instance of file", isFileUploaded instanceof File)
+      const data = new FormData();
+      data.append('files', action.profilePayload.files[0]);
+      data.append('refId', editPayload.id);
+      data.append('ref', 'profile');
+      data.append('field', 'avatar');
 
-      if (isFileUploaded instanceof File && avatarId) {
-        const deleteResponse = yield call(api.deleteProfileImage, editProfileResponse.avatar.id);
+      const avatarUploadResponse = yield call(api.uploadProfileAvatar, data);
 
-        if (deleteResponse) {
-          const data = new FormData();
-          data.append('files', action.profilePayload.files[0]);
-          data.append('refId', editProfileResponse.id);
-          data.append('ref', 'profile');
-          data.append('field', 'avatar');
+      console.log("avatarUploadResponse", avatarUploadResponse);
+      const editProfileResponse = yield call(api.editUserProfile,  action.profileId, editPayload);
+      console.log("editProfileResponse", editProfileResponse)
 
-          const avatarUploadResponse = yield call(api.uploadProfileAvatar, data);
-          if (avatarUploadResponse && deleteResponse) {
-            userProfile = { ...userProfile, avatar: avatarUploadResponse[0]}
-            yield put(onProfileEditSuccess(userProfile));
-          }
+      // update user profileCompleted: true once profile is created
+      const profileUpdateResponse = yield call(api.updateUserProfile, userId, profileCompletePayload);
+      console.log("profileUpdateResponse", profileUpdateResponse)
+      const { email, confirmed, profileCompleted, isbusiness, id } = profileUpdateResponse;
+      const userFieldsLocallyStored = { email, confirmed, profileCompleted, isbusiness, id}
+
+      if (avatarUploadResponse && editProfileResponse) {
+        console.log("IF avatarUploadResponse && editProfileResponse")
+        const userProfile = {
+          id: editProfileResponse.id,
+          businessName: editProfileResponse.businessName,
+          businessId: editProfileResponse.businessId,
+          website: editProfileResponse.website,
+          description: editProfileResponse.description,
+          phoneNumber: editProfileResponse.phoneNumber,
+          facebookLink: editProfileResponse.facebookLink,
+          twitterLink: editProfileResponse.twitterLink,
+          linkedinLink: editProfileResponse.linkedinLink,
+          address: editProfileResponse.address,
+          avatar: {...avatarUploadResponse[0]},
         }
-      } else {
-        const data = new FormData();
-        data.append('files', action.profilePayload.files[0]);
-        data.append('refId', editProfileResponse.id);
-        data.append('ref', 'profile');
-        data.append('field', 'avatar');
-        const avatarUploadResponse = yield call(api.uploadProfileAvatar, data);
-        if (avatarUploadResponse) {
-          yield put(onProfileEditSuccess(userProfile));
-        }
+        console.log("Store userProfile", userProfile)
+        yield call(auth.setUserInfo, userFieldsLocallyStored);
+        yield put(onProfileEditSuccess(userProfile));
       }
-      yield put(onProfileEditSuccess(userProfile));
+    } else {
+      console.log("else")
+      const editProfileResponse = yield call(api.editUserProfile,  action.profileId, editPayload);
+      console.log("editProfileResponse", editProfileResponse)
+      if (editProfileResponse) {
+        const userProfile = {
+          id: editProfileResponse.id,
+          businessName: editProfileResponse.businessName,
+          businessId: editProfileResponse.businessId,
+          website: editProfileResponse.website,
+          description: editProfileResponse.description,
+          phoneNumber: editProfileResponse.phoneNumber,
+          facebookLink: editProfileResponse.facebookLink,
+          twitterLink: editProfileResponse.twitterLink,
+          linkedinLink: editProfileResponse.linkedinLink,
+          address: editProfileResponse.address,
+          avatar: editProfileResponse.avatar,
+        }
+        yield put(onProfileEditSuccess(userProfile));
+      }
     }
+
   } catch(error) {
-    yield put(onProfileEditFailed(error));
+    yield put(onUserProfileDeleteFailed(error));
+  }
+
+}
+
+export function* avatarDeleteWatcher(action) {
+  console.log("Avatar delete action", action);
+  try {
+    const deleteResponse = yield call(api.deleteProfileImage, action.avatarId);
+    if (deleteResponse) {
+      // const userId = auth.get('userInfo') && auth.get('userInfo').id;
+      // const profileCompletePayload = { profileCompleted: false };
+      // const profileUpdateResponse = yield call(api.updateUserProfile, userId, profileCompletePayload);
+      // const { email, confirmed, profileCompleted, isbusiness, id } = profileUpdateResponse;
+      // const userFieldsLocallyStored = { email, confirmed, profileCompleted, isbusiness, id}
+      const profileApiResponse = yield call(api.getUserProfile, action.profileId);
+
+      if (profileApiResponse ) {
+        const userProfile = {
+          id: profileApiResponse.id,
+          businessName: profileApiResponse.businessName,
+          businessId: profileApiResponse.businessId,
+          website: profileApiResponse.website,
+          description: profileApiResponse.description,
+          phoneNumber: profileApiResponse.phoneNumber,
+          facebookLink: profileApiResponse.facebookLink,
+          twitterLink: profileApiResponse.twitterLink,
+          linkedinLink: profileApiResponse.linkedinLink,
+          address: profileApiResponse.address,
+          avatar: {},
+        }
+        console.log("aftre avatar delete userProfile status", userProfile)
+
+        // yield call(auth.setUserInfo, userFieldsLocallyStored);
+        yield put(onUserProfileDeleteSuccess());
+        yield put(onProfileEditSuccess(userProfile));
+        // yield call(forwardTo, '/dashboard/profile');
+        yield put(push('/dashboard/profile'));
+      }
+
+    }
+  } catch {
+    yield put(onUserProfileDeleteFailed());
   }
 }
 
@@ -175,4 +234,13 @@ export default function* defaultSaga() {
   yield takeLatest(ON_PROFILEPAGE_SAVE, profileSaveWatcher);
   yield takeLatest(ON_PROFILEPAGE_LOAD, profileLoadWatcher);
   yield takeLatest(ON_PROFILE_EDIT, profileEditWatcher);
+  yield takeLatest(ON_USERPROFILE_DELETE, avatarDeleteWatcher)
 }
+
+/**
+ * Helper to handle navigation from sagas.
+ * @param  {String} location The path to navigate
+//  */
+// function forwardTo(location) {
+//   history.push(location);
+// }
