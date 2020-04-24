30661a9e5f759952a15d14d17c2013346ccf07d3
Thaman <thaman.chand@invian.fi>
2020-04-24 12:10:17 +0545

    ON_BOOKING_LOAD, ON_BOOKING_LOAD_SUCCESS, ON_BOOKING_LOAD_FAILED

---
+++ app/containers/Booking/constants.js
@@ -0,0 +1,3 @@
+export const ON_BOOKING_LOAD = 'app/BookingPage/ON_BOOKING_LOAD';
+export const ON_BOOKING_LOAD_SUCCESS = 'app/BookingPage/ON_BOOKING_LOAD_SUCCESS';
+export const ON_BOOKING_LOAD_FAILED = 'app/BookingPage/ON_BOOKING_LOAD_FAILED';
