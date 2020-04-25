f1c39846c71b98c555b901777d1d795da86db1e3
Thaman <thaman.chand@invian.fi>
2020-04-25 09:47:21 +0545

    getResources api util

--- app/containers/Booking/api.js
+++ app/containers/Booking/api.js
@@ -5,9 +5,21 @@ import auth from '../../utils/auth';
 const token = auth.getToken();
 
 const BASE_URL = 'http://localhost:1337/';
-// data api calls
+
+// get Bookings
 export const getBookings = () => axios
   .get(`${BASE_URL}${'bookings'}`, {
+    headers: {
+      'Accept': 'application/json',
+      'Authorization': `Bearer ${token}`,
+    }
+  }).then(response => response.data).catch(err => {
+    throw err;
+  });
+
+// get Resources
+export const getResources = () => axios
+  .get(`${BASE_URL}${'resources'}`, {
     headers: {
       'Accept': 'application/json',
       'Authorization': `Bearer ${token}`,
