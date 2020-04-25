3ac49ea9156acfbcfc74f6764df5dde15385b903
Thaman <thaman.chand@invian.fi>
2020-04-25 09:45:10 +0545

    ON_RESOURCE_LOAD, ON_RESOURCE_LOAD_SUCCESS, ON_RESOURCE_LOAD_FAILED constant init

--- app/containers/Booking/constants.js
+++ app/containers/Booking/constants.js
@@ -1,3 +1,7 @@
 export const ON_BOOKING_LOAD = 'app/BookingPage/ON_BOOKING_LOAD';
 export const ON_BOOKING_LOAD_SUCCESS = 'app/BookingPage/ON_BOOKING_LOAD_SUCCESS';
 export const ON_BOOKING_LOAD_FAILED = 'app/BookingPage/ON_BOOKING_LOAD_FAILED';
+
+export const ON_RESOURCE_LOAD = 'app/BookingPage/ON_RESOURCE_LOAD';
+export const ON_RESOURCE_LOAD_SUCCESS = 'app/BookingPage/ON_RESOURCE_LOAD_SUCCESS';
+export const ON_RESOURCE_LOAD_FAILED = 'app/BookingPage/ON_BOOKING_LOAD_FAILED';
