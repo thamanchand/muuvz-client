8c635daea7e2f9e2cd7e8d6a917a340a1ff53a43
Thaman <thaman.chand@invian.fi>
2020-04-27 11:30:57 +0545

    rearrange search bar on top and resource row to full width

--- app/containers/Listing/index.js
+++ app/containers/Listing/index.js
@@ -4,7 +4,8 @@ import { connect } from 'react-redux';
 import { Container, Row, Col } from 'reactstrap';
 import { createStructuredSelector } from 'reselect';
 import { bindActionCreators, compose } from 'redux';
-
+import { Form, Field } from 'react-final-form';
+import TimetableIcon from 'mdi-react/TimetableIcon';
 
 // Utils
 import injectSaga from 'utils/injectSaga';
@@ -17,12 +18,15 @@ import {
   isSearchLoadingSelector,
   isBookedSelector,
 } from './selectors';
-import Search from '../../shared/SearchForm';
+
 import VanListing from './components/VanListing';
 import saga from './saga';
 import reducer from './reducers';
 
 import HeaderNav from '../../shared/Header';
+import renderDateTimePickerField from '../../shared/DateTimePicker/index';
+import renderCheckBoxField from '../../shared/Checkbox/index';
+import Error from '../../shared/ErrorField';
 
 const key = 'listingPage';
 
@@ -60,36 +64,130 @@ class VanListPage extends PureComponent {
     return (
       <>
         <HeaderNav source="listing" />
+
         <div className="Listing__page">
+          <div className="col-lg-12">
+
+            <div className="row">
+              <div
+                className="editbtn__wrapper"
+              >
+                <span
+                  role="button"
+                  tabIndex="0"
+                  onClick={this.onSearchEditToggle}
+                  onKeyDown={this.onSearchEditToggle}
+                  className="edit-btn"
+                >
+                  Edit search
+                </span>
+              </div>
+              <Form
+                validate={values => { // validate both passowrds are same
+                  const errors = {};
+                  if (!values.location) {
+                    errors.location = 'Location is required';
+                  }
+                  if (!values.pickupDateTime) {
+                    errors.pickupDateTime = 'Pick up datetime is required';
+                  }
+                  if (!values.dropOfftDateTime) {
+                    errors.dropOfftDateTime = 'Drop off datetime is required';
+                  }
+                  return errors
+                }}
+                onSubmit={(values) => onSearch(values)}
+                initialValues={storedValues}
+                render={({ handleSubmit, values }) => (
+                  <form className="form" onSubmit={handleSubmit} style={{backgroundColor: '#FFF'}}>
+                    <div className="col-lg-3 col-md-3 col-sm-12 resource__searchbar">
+                      <div className="form__form-group">
+                        <span className="form__form-group-label">Pickup location</span>
+                        <div className="form__form-group-field">
+                          <Field
+                            name="location"
+                            component="input"
+                            type="text"
+                            placeholder="Helsinki"
+                            required
+                            disabled={!isEdit}
+                          />
+                        </div>
+                        <Error name="location" />
+                      </div>
+                    </div>
+
+                    <div className="col-lg-3 col-md-3 col-sm-12 resource__searchbar">
+                      <div className="form__form-group">
+                        <span className="form__form-group-label">Pickup date & time</span>
+                        <div className="form__form-group-field">
+                          <Field
+                            name="pickupDateTime"
+                            component={renderDateTimePickerField}
+                            disabled={!isEdit}
+                          />
+                          <div className="form__form-group-icon">
+                            <TimetableIcon />
+                          </div>
+                        </div>
+                        <Error name="pickupDateTime" />
+                      </div>
+                    </div>
+
+                    <div className="col-lg-3 col-md-3 col-sm-12 resource__searchbar">
+                      <div className="form__form-group">
+                        <span className="form__form-group-label">Drop-off date & time</span>
+                        <div className="form__form-group-field">
+                          <Field
+                            name="dropOfftDateTime"
+                            component={renderDateTimePickerField}
+                            disabled={!isEdit}
+                          />
+                          <div className="form__form-group-icon">
+                            <TimetableIcon />
+                          </div>
+                        </div>
+                        <Error name="dropOfftDateTime" />
+                      </div>
+                    </div>
+                    <div className="col-lg-3 col-md-3 col-sm-12 resource__searchbar">
+                      <div className="form__form-group">
+                        <div className="form__form-group-field resource__page__checkbox">
+                          <Field
+                            name="dropAtPickUpLocation"
+                            component={renderCheckBoxField}
+                            color="#646777"
+                            className="checkbox-btn  checkbox-btn--colored"
+                            label="Drop-off at pickup location"
+                            disabled={!isEdit}
+                          />
+                          <button
+                            className="rounded btn btn-primary search-btn"
+                            type="button"
+                            onClick={() => handleSubmit(values)}
+                            disabled={!isEdit}
+                            storedValues={storedValues}
+                          >
+                            Search
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  </form>
+                )}
+              />
+            </div>
+          </div>
+
           <Container>
             <Row>
-              <Col md={3} className="van__search">
-                <div className="van__list-nav">
-                  <div
-                    className="edit-btn"
-                    role="button"
-                    tabIndex="0"
-                    onClick={this.onSearchEditToggle}
-                    onKeyDown={this.onSearchEditToggle}
-                  >
-                    Edit search
-                  </div>
-                  <div className="search__wrapper">
-                    <Search
-                      onSearch={this.onSearch}
-                      disabled={isEdit}
-                      storedValues={storedValues}
-                      isBooked={isBooked}
-                    />
-                  </div>
-                </div>
-              </Col>
-              <Col md={9} className="van__content">
+              <Col md={12} className="van__content">
                 <div className="van__list">
                   <VanListing
                     vanList={resourceList}
                     bookingHandler={this.bookingHandler}
                     isSearchLoading={isSearchLoading}
+                    isBooked={isBooked}
                   />
                 </div>
               </Col>
