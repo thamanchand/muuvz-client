cc7704c4383d6871a47f3c3e1c5258b79a3855d4
Thaman <thaman.chand@invian.fi>
2020-04-28 21:08:18 +0545

    show phone and email info in bookingconfirmation page

--- app/containers/BookingConfirmation/index.js
+++ app/containers/BookingConfirmation/index.js
@@ -6,6 +6,8 @@ import {
   Card, CardBody, Col, Row
 } from 'reactstrap';
 import CheckboxMarkedCircleIcon from 'mdi-react/CheckboxMarkedCircleIcon';
+import EmailIcon from 'mdi-react/EmailIcon';
+import CellphoneIcon from 'mdi-react/CellphoneIcon';
 
 import Footer from '../HomePage/components/Footer';
 import HeaderNav from '../../shared/Header';
@@ -40,8 +42,29 @@ class EmailConfirmationCard extends PureComponent {
                             <CheckboxMarkedCircleIcon className="email-confirmation__check" />
                           </div>
                           <h3 className="email-confirmation__title">Booking ID: {(location.search).split('=')[1]} </h3>
-                          <p className="email-confirmation__sub">You successfully created booking</p>
+                          <p className="email-confirmation__sub">Your booking has been requested</p>
                           <p className="email-confirmation__sub">Please your check email for booking details</p>
+                          <p className="email-confirmation__sub">If you have any questions please contact us at</p>
+                          <div className="contact__list">
+                            <ul className="list list--icon">
+                              <li className="list__item">
+                                <span className="contact_icon">
+                                  <EmailIcon size="20" color="#ff4861" />
+                                </span>
+                                <span className="contact_label">
+                                  support@muvzz.fi
+                                </span>
+                              </li>
+                              <li className="list__item">
+                                <span className="contact_icon">
+                                  <CellphoneIcon size="20" color="#ff4861" />
+                                </span>
+                                <span className="contact_label">
+                                  +358442106670
+                                </span>
+                              </li>
+                            </ul>
+                          </div>
                         </div>
                       </CardBody>
                     </Card>
