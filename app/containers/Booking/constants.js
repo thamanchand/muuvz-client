8dcca18b31ea63be4da19812d7e7eaa2502572f9
Thaman <thaman.chand@invian.fi>
2020-05-02 21:18:28 +0545

    ON_BOOKING_ACCEPT, ON_BOOKING_ACCEPT_SUCCESS,ON_BOOKING_ACCEPT_FAILED,ON_BOOKING_CANCEL,ON_BOOKING_CANCEL_SUCCESS,ON_BOOKING_CANCEL_FAILED constants init

--- app/containers/Booking/constants.js
+++ app/containers/Booking/constants.js
@@ -4,4 +4,12 @@ export const ON_BOOKING_LOAD_FAILED = 'app/BookingPage/ON_BOOKING_LOAD_FAILED';
 
 export const ON_RESOURCE_LOAD = 'app/BookingPage/ON_RESOURCE_LOAD';
 export const ON_RESOURCE_LOAD_SUCCESS = 'app/BookingPage/ON_RESOURCE_LOAD_SUCCESS';
-export const ON_RESOURCE_LOAD_FAILED = 'app/BookingPage/ON_BOOKING_LOAD_FAILED';
+export const ON_RESOURCE_LOAD_FAILED = 'app/BookingPage/ON_RESOURCE_LOAD_FAILED';
+
+export const ON_BOOKING_ACCEPT = 'app/BookingPage/ON_BOOKING_ACCEPT';
+export const ON_BOOKING_ACCEPT_SUCCESS = 'app/BookingPage/ON_BOOKING_ACCEPT_SUCCESS';
+export const ON_BOOKING_ACCEPT_FAILED = 'app/BookingPage/ON_BOOKING_ACCEPT_FAILED';
+
+export const ON_BOOKING_CANCEL = 'app/BookingPage/ON_BOOKING_CANCEL';
+export const ON_BOOKING_CANCEL_SUCCESS = 'app/BookingPage/ON_BOOKING_CANCEL_SUCCESS';
+export const ON_BOOKING_CANCEL_FAILED = 'app/BookingPage/ON_BOOKING_CANCEL_FAILED';
