635f4a2a097425d00c53d03b204455a151e39097
Thaman <thaman.chand@invian.fi>
2020-05-03 11:26:13 +0545

    set selected nav initialState based on isProfileCompleted and manage CHANGE_SIDENAV_SELECTED state

--- app/containers/Layout/reducer.js
+++ app/containers/Layout/reducer.js
@@ -1,11 +1,17 @@
 import {
   CHANGE_SIDEBAR_VISIBILITY,
   CHANGE_MOBILE_SIDEBAR_VISIBILITY,
+  CHANGE_SIDENAV_SELECTED,
 } from './constants';
+
+import auth from '../../utils/auth';
+
+const isProfileCompleted = auth.get('userInfo') && auth.get('userInfo').profileCompleted;
 
 const initialState = {
   show: false,
   collapse: false,
+  selected: isProfileCompleted ? 'Booking' : 'Profile',
 };
 
 const layoutReducer = (state = initialState, action) => {
@@ -14,6 +20,10 @@ const layoutReducer = (state = initialState, action) => {
       return { ...state, collapse: !state.collapse };
     case CHANGE_MOBILE_SIDEBAR_VISIBILITY:
       return { ...state, show: !state.show };
+
+    case CHANGE_SIDENAV_SELECTED:
+      return { ...state, selected: action.activeNav };
+
     default:
       return state;
   }
