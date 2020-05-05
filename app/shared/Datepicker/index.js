614dc3447a174920e8ba1fc56371c8f3697f4334
Thaman <thaman.chand@invian.fi>
2020-05-05 16:53:00 +0545

    modify Datepicker so it can take input, disabled, meat, endDateChanged, startDateChanged props

--- app/shared/Datepicker/index.js
+++ app/shared/Datepicker/index.js
@@ -4,9 +4,6 @@ import { isMobileOnly } from 'react-device-detect';
 import PropTypes from 'prop-types';
 
 class DatePickerField extends PureComponent {
-  static propTypes = {
-    onChange: PropTypes.func.isRequired,
-  };
 
   constructor() {
     super();
@@ -16,36 +13,67 @@ class DatePickerField extends PureComponent {
     this.handleChange = this.handleChange.bind(this);
   }
 
-  handleChange(date) {
-    const { onChange } = this.props;
+  handleChange = date => {
+    const { input, startDateChanged, endDateChanged } = this.props;
     this.setState({
       startDate: date,
     });
-    onChange(date);
-  }
+
+    if (input.name === 'pickupDate') {
+      input.onChange(date);
+      startDateChanged(date)
+    }
+    else if (input.name === 'dropOffDate') {
+      input.onChange(date);
+      endDateChanged(date)
+    }
+  };
 
   render() {
     const { startDate } = this.state;
+    const { disabled, input } = this.props;
+    const initialDate = input.value ? Date.parse(input.value) : startDate;
 
     return (
       <div className="date-picker">
         <DatePicker
           className="form__form-group-datepicker"
-          selected={startDate}
+          selected={initialDate}
           onChange={this.handleChange}
-          dateFormat="yyyy/MM/dd"
+          dateFormat="dd.MM.yyyy"
           dropDownMode="select"
           popperPlacement="center"
           withPortal={isMobileOnly}
-        />
+          disabled={disabled}
+          minDate={new Date()}
+          required
+        >
+          <div className="select__time">Please select time</div>
+        </DatePicker>
       </div>
     );
   }
 }
 
+DatePickerField.propTypes = {
+  input: PropTypes.shape({
+    onChange: PropTypes.func,
+    name: PropTypes.string,
+  }).isRequired,
+  disabled: PropTypes.bool,
+  startDateChanged: PropTypes.func,
+  endDateChanged: PropTypes.func,
+}
+
 const renderDatePickerField = (props) => {
-  const { input } = props;
-  return <DatePickerField {...input} />;
+  const { input, disabled, meta, endDateChanged, startDateChanged } = props;
+  return <DatePickerField
+    input={input}
+    disabled={disabled}
+    meta={meta}
+    endDateChanged={endDateChanged}
+    startDateChanged={startDateChanged}
+  />;
 };
 
 renderDatePickerField.propTypes = {
@@ -53,6 +81,10 @@ renderDatePickerField.propTypes = {
     onChange: PropTypes.func,
     name: PropTypes.string,
   }).isRequired,
+  disabled: PropTypes.bool.isRequired,
+  meta: PropTypes.object,
+  startDateChanged: PropTypes.func,
+  endDateChanged: PropTypes.func,
 };
 
 export default renderDatePickerField;
