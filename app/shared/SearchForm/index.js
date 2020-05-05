db443d94bb9097ab5b10beba0769fa03d9d7996a
Thaman <thaman.chand@invian.fi>
2020-05-05 16:57:57 +0545

    separate date and time picker, combined date and time values, calculation duration

--- app/shared/SearchForm/index.js
+++ app/shared/SearchForm/index.js
@@ -1,7 +1,9 @@
-import React, { useRef, useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Form, Field } from 'react-final-form';
-import TimetableIcon from 'mdi-react/TimetableIcon';
+import ClockOutlineIcon from 'mdi-react/ClockOutlineIcon';
+import EventIcon from 'mdi-react/EventIcon';
+
 import moment from 'moment';
 import { Col } from 'reactstrap';
 
@@ -10,16 +12,19 @@ import Error from '../ErrorField';
 import renderDatePickerField from '../Datepicker';
 import renderTimePickerFied from '../TimePicker';
 
+const humanizeDuration = require('humanize-duration')
+
+
 // import renderCheckBoxField from '../Checkbox/index';
 
 // custom hook for getting previous value
-function usePrevious(value) {
-  const ref = useRef();
-  useEffect(() => {
-    ref.current = value;
-  });
-  return ref.current;
-}
+// function usePrevious(value) {
+//   const ref = useRef();
+//   useEffect(() => {
+//     ref.current = value;
+//   });
+//   return ref.current;
+// }
 
 const Search = ({ onSearch, disabled, storedValues }) => {
   const [duration, setDuration] = React.useState();
@@ -27,31 +32,69 @@ const Search = ({ onSearch, disabled, storedValues }) => {
   const [bookingEndDateTime, setBookingEndDateTime] = React.useState();
   const [isStartDateTimeGreater, setIsStartDateTimeGreater] = React.useState(false);
 
-  const prevBookingStartDateTime = usePrevious(bookingStartDateTime);
-  const prevBookingEndDateTime = usePrevious(bookingEndDateTime);
+  // const prevBookingStartDateTime = usePrevious(bookingStartDateTime);
+  // const prevBookingEndDateTime = usePrevious(bookingEndDateTime);
+  //
 
+  // const endDateChange = (endDateTime) => {
+  //   setBookingEndDateTime(moment(moment(endDateTime)).format('YYYY-MM-DDTHH:mm:ss'));
+  //   setIsStartDateTimeGreater(moment(bookingStartDateTime).isAfter(bookingEndDateTime));
+  //
+  //   // calculate number of hours
+  //   const bookingHours = moment
+  //     .duration(moment(endDateTime, 'YYYY-MM-DDTHH:mm:ss')
+  //       .diff(moment(prevBookingStartDateTime, 'YYYY-MM-DDTHH:mm:ss.SSS'))
+  //     ).asHours();
+  //   setDuration(bookingHours);
+  // }
+  // const startDateChange = (startDateTime) => {
+  //   setBookingStartDateTime(moment(moment(startDateTime)).format('YYYY-MM-DDTHH:mm:ss'));
+  //   setIsStartDateTimeGreater(moment(bookingStartDateTime).isAfter(bookingEndDateTime));
+  //   // calculate number of hours
+  //   const bookingHours = moment
+  //     .duration(moment(moment, 'YYYY-MM-DDTHH:mm:ss')
+  //       .diff(moment(prevBookingEndDateTime, 'YYYY-MM-DDTHH:mm:ss'))
+  //     ).asHours();
+  //   setDuration(bookingHours);
+  // }
+  //
+  const startDateChanged = (startDate) => {
+    setBookingStartDateTime(moment(moment(startDate)).format('YYYY-MM-DD'));
+  }
 
-  const endDateChange = (endDateTime) => {
-    setBookingEndDateTime(moment(moment(endDateTime)).format('YYYY-MM-DDTHH:mm:ss'));
+  const endDateChanged = (endDate) => {
+    setBookingEndDateTime(moment(moment(endDate)).format('YYYY-MM-DD'));
+  }
+
+  const endTimeChanged = (endTime) => {
+    // format End Time
+    const formatEndTime = moment(endTime).format('HH:mm');
+    // combined EndDate with endTime
+    const combinedWithEndDate = moment(`${moment(bookingEndDateTime).format('YYYY-MM-DD')}${formatEndTime}`, 'YYYY-MM-DDTHH:mm:ss');
+    setBookingEndDateTime(moment(combinedWithEndDate).format('YYYY-MM-DDTHH:mm'));
+    // check if startDateTime is greater than endDateTime
     setIsStartDateTimeGreater(moment(bookingStartDateTime).isAfter(bookingEndDateTime));
 
     // calculate number of hours
-    const bookingHours = moment
-      .duration(moment(endDateTime, 'YYYY-MM-DDTHH:mm:ss')
-        .diff(moment(prevBookingStartDateTime, 'YYYY-MM-DDTHH:mm:ss.SSS'))
-      ).asHours();
+    const bookingHours = moment(bookingEndDateTime)
+      .diff(moment(bookingStartDateTime, 'YYYY-MM-DDTHH:mm'));
     setDuration(bookingHours);
   }
-  const startDateChange = (startDateTime) => {
-    setBookingStartDateTime(moment(moment(startDateTime)).format('YYYY-MM-DDTHH:mm:ss'));
+
+  const startTimeChanged = (startTime) => {
+    // format startTime HH:mm
+    const formatStartTime = moment(startTime).format('HH:mm');
+    // combined startDate with startTime
+    const combinedWithStartDate = moment(`${moment(bookingStartDateTime).format('YYYY-MM-DD')}${formatStartTime}`, 'YYYY-MM-DDTHH:mm');
+    setBookingStartDateTime(moment(combinedWithStartDate).format('YYYY-MM-DDTHH:mm'));
     setIsStartDateTimeGreater(moment(bookingStartDateTime).isAfter(bookingEndDateTime));
+
     // calculate number of hours
-    const bookingHours = moment
-      .duration(moment(moment, 'YYYY-MM-DDTHH:mm:ss')
-        .diff(moment(prevBookingEndDateTime, 'YYYY-MM-DDTHH:mm:ss'))
-      ).asHours();
+    const bookingHours = moment(bookingEndDateTime)
+      .diff(moment(bookingStartDateTime, 'YYYY-MM-DDTHH:mm'));
     setDuration(bookingHours);
   }
+
   return (
     <Form
       validate={values => { // validate both passowrds are same
@@ -59,11 +102,17 @@ const Search = ({ onSearch, disabled, storedValues }) => {
         if (!values.location) {
           errors.location = 'Location is required';
         }
-        if (!values.pickupDateTime) {
-          errors.pickupDateTime = 'Pick up datetime is required';
+        if (!values.pickupDate) {
+          errors.pickupDate = 'Pick up date is required';
         }
-        if (!values.dropOfftDateTime) {
-          errors.dropOfftDateTime = 'Drop off datetime is required';
+        if (!values.pickupTime) {
+          errors.pickupTime = 'Pikup time is required';
+        }
+        if (!values.dropOffDate) {
+          errors.dropOffDate = 'Drop off date is required';
+        }
+        if (!values.dropOffTime) {
+          errors.dropOffTime = 'Drop off time is required';
         }
         return errors
       }}
@@ -104,14 +153,17 @@ const Search = ({ onSearch, disabled, storedValues }) => {
               <span className="form__form-group-label">Pickup date</span>
               <div className="form__form-group-field">
                 <Field
-                  name="pickupDateTime"
+                  name="pickupDate"
                   component={renderDatePickerField}
                   disabled={!disabled}
-                  startDateChanged={(startDateTime) => startDateChange(startDateTime)}
+                  startDateChanged={(startDate) => startDateChanged(startDate)}
                   className="search__field_input"
                 />
-
+                <div className="form__form-group-icon">
+                  <EventIcon />
+                </div>
               </div>
+              <Error name="pickupDate" />
             </div>
           </Col>
           <Col className="col-lg-6 col-md-12  col-sm-12 col-12 startdatepicker__col startimepicker__col">
@@ -119,59 +171,65 @@ const Search = ({ onSearch, disabled, storedValues }) => {
               <span className="form__form-group-label">Pickup time</span>
               <div className="form__form-group-field">
                 <Field
-                  name="pickupDateTime"
+                  name="pickupTime"
                   component={renderTimePickerFied}
                   disabled={!disabled}
-                  startDateChanged={(startDateTime) => startDateChange(startDateTime)}
+                  startTimeChanged={(startTime) => startTimeChanged(startTime)}
                   className="search__field_input"
                 />
-
+                <div className="form__form-group-icon">
+                  <ClockOutlineIcon />
+                </div>
               </div>
-              <Error name="pickupDateTime" />
+              <Error name="pickupTime" />
             </div>
           </Col>
           <Col className="col-lg-6 col-md-12  col-sm-12 col-12 startdatepicker__col">
             <div className="form__form-group">
-              <span className="form__form-group-label">Drop-off date & time</span>
+              <span className="form__form-group-label">Drop-off date</span>
               <div className="form__form-group-field">
                 <Field
-                  name="dropOfftDateTime"
+                  name="dropOffDate"
                   component={renderDatePickerField}
                   disabled={!disabled}
-                  endDateChanged={(endDateTime) => endDateChange(endDateTime)}
+                  endDateChanged={(endDate) => endDateChanged(endDate)}
                 />
                 <div className="form__form-group-icon">
-                  <TimetableIcon />
+                  <EventIcon />
                 </div>
               </div>
-              <Error name="dropOfftDateTime" />
+              <Error name="dropOffDate" />
             </div>
           </Col>
           <Col className="col-lg-6 col-md-12  col-sm-12 col-12 startdatepicker__col startimepicker__col">
             <div className="form__form-group form-pickuptime">
-              <span className="form__form-group-label">Pickup time</span>
+              <span className="form__form-group-label">Drop-off time</span>
               <div className="form__form-group-field">
                 <Field
-                  name="pickupDateTime"
+                  name="dropOffTime"
                   component={renderTimePickerFied}
                   disabled={!disabled}
-                  startDateChanged={(startDateTime) => startDateChange(startDateTime)}
+                  endTimeChanged={(endTime) => endTimeChanged(endTime)}
                   className="search__field_input"
                 />
-
+                <div className="form__form-group-icon">
+                  <ClockOutlineIcon />
+                </div>
               </div>
-              <Error name="pickupDateTime" />
+              <Error name="dropOffTime" />
             </div>
           </Col>
           <div className="form__form-group">
             {isStartDateTimeGreater && (
-              <span className="validation__error">
-                Booking start time should be greater
-              </span>
+              <div className="search__error">
+                Booking start date and time should be greater
+              </div>
             )
             }
-            <p>
-              {duration}
+            <p className="booking__duration">
+              {values.dropOffTime && values.pickupTime && (
+                humanizeDuration(duration,  { language: 'fi' })
+              )}
             </p>
             <div className="form__form-group-field">
               <button
