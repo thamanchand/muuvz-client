80840e8f86e7e6a0e593fe776df9092750e78223
Thaman <thaman.chand@invian.fi>
2020-05-07 10:30:02 +0545

    display user avatar

--- app/containers/Layout/topbar/Topbar.js
+++ app/containers/Layout/topbar/Topbar.js
@@ -5,7 +5,7 @@ import TopbarSidebarButton from './TopbarSidebarButton';
 import TopbarProfile from './TopbarProfile';
 import TopbarLanguage from './TopbarLanguage';
 
-const Topbar = ({ changeMobileSidebarVisibility, changeSidebarVisibility }) => (
+const Topbar = ({ changeMobileSidebarVisibility, changeSidebarVisibility, userProfile }) => (
   <div className="topbar">
     <div className="topbar__wrapper">
       <div className="topbar__left">
@@ -16,7 +16,7 @@ const Topbar = ({ changeMobileSidebarVisibility, changeSidebarVisibility }) => (
         <Link className="topbar__logo" to="/" />
       </div>
       <div className="topbar__right">
-        <TopbarProfile />
+        <TopbarProfile userAvatar={userProfile}/>
         <TopbarLanguage />
       </div>
     </div>
@@ -26,6 +26,7 @@ const Topbar = ({ changeMobileSidebarVisibility, changeSidebarVisibility }) => (
 Topbar.propTypes = {
   changeMobileSidebarVisibility: PropTypes.func.isRequired,
   changeSidebarVisibility: PropTypes.func.isRequired,
+  userProfile: PropTypes.object,
 };
 
 export default Topbar;
--- app/containers/Layout/topbar/TopbarProfile.js
+++ app/containers/Layout/topbar/TopbarProfile.js
@@ -1,18 +1,26 @@
 import React from 'react';
-import avatar from '../../../assets/images/topbar/ava.png';
+import PropTypes from 'prop-types';
 
+import avatar from '../../../assets/images/topbar/ava.png';
 import auth from '../../../utils/auth';
 
-const TopbarProfile = () => {
+const TopbarProfile = ({ userAvatar }) => {
+  const userImage = userAvatar && userAvatar.avatar &&  userAvatar.avatar.url
+    ? `${'http://localhost:1337'}${userAvatar.avatar.url}`
+    : avatar
   const userEmail = auth.get('userInfo') && auth.get('userInfo').email;
   return (
     <div className="topbar__profile">
       <button type="button" className="topbar__avatar">
-        <img className="topbar__avatar-img" src={avatar} alt="avatar" />
+        <img className="topbar__avatar-img" src={userImage}  alt="avatar" />
         <p className="topbar__avatar-name">{userEmail}</p>
       </button>
     </div>
   )
+}
+
+TopbarProfile.propTypes = {
+  userAvatar: PropTypes.object,
 }
 
 export default TopbarProfile;
