81673fc93afbeaab38426e9add265556efc0deea
Thaman <thaman.chand@invian.fi>
2020-05-07 10:58:46 +0545

    fix modal footer buttons

--- app/containers/Resources/components/DeleteResourceModal.js
+++ app/containers/Resources/components/DeleteResourceModal.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Row } from 'reactstrap';
+import { Col, Row } from 'reactstrap';
 
 import Modal from '../../../shared/Modal'
 
@@ -14,21 +14,23 @@ const DeleteResourceModal = ({ show, onClose, deleteResourceAction }) => (
     header
   >
     <Row>
-      <p className="delete__modal__label">Are you sure you want to delete this resource?</p>
-      <div className="delete__confirmation">
-        <span
-          role="presentation"
-          className="cancel__button"
-          onClick={onClose}
-        > cancel</span>
-        <button
-          className="square btn btn-danger btn-sm rounded"
-          type="submit"
-          onClick={deleteResourceAction}
-        >
-          Delete resource
-        </button>
-      </div>
+      <Col md={12} sm={12}>
+        <p className="delete__modal__label">Are you sure you want to delete this resource?</p>
+        <div className="delete__confirmation">
+          <span
+            role="presentation"
+            className="cancel__button"
+            onClick={onClose}
+          > Cancel</span>
+          <button
+            className="square btn btn-danger btn-sm rounded"
+            type="submit"
+            onClick={deleteResourceAction}
+          >
+            Delete resource
+          </button>
+        </div>
+      </Col>
     </Row>
   </Modal>
 )
