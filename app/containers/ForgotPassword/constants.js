07809171178b4b0b561680a12413999b65d08cc3
Thaman <thaman.chand@invian.fi>
2020-05-08 09:08:45 +0545

    ON_PASSWORD_FORGOT, ON_PASSWORD_FORGOT_SUCCESS, ON_PASSWORD_FORGOT_FAILED constants

---
+++ app/containers/ForgotPassword/constants.js
@@ -0,0 +1,9 @@
+/*
+ *
+ * RegisterPage constants
+ *
+ */
+
+export const ON_PASSWORD_FORGOT = 'app/ForgotPage/ON_PASSWORD_FORGOT';
+export const ON_PASSWORD_FORGOT_SUCCESS = 'app/ForgotPage/ON_PASSWORD_FORGOT_SUCCESS';
+export const ON_PASSWORD_FORGOT_FAILED = 'app/ForgotPage/ON_PASSWORD_FORGOT_FAILED';
