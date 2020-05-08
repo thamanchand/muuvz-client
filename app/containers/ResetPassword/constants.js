0f9e6b5819511547ff76516ef8d6881bf6a0ec82
Thaman <thaman.chand@invian.fi>
2020-05-08 09:12:05 +0545

    ON_PASSWORD_RESET, ON_PASSWORD_RESET_SUCCESS, ON_PASSWORD_RESET_FAILED constants init

---
+++ app/containers/ResetPassword/constants.js
@@ -0,0 +1,9 @@
+/*
+ *
+ * RegisterPage constants
+ *
+ */
+
+export const ON_PASSWORD_RESET = 'app/RegisterPage/ON_PASSWORD_RESET';
+export const ON_PASSWORD_RESET_SUCCESS = 'app/RegisterPage/ON_PASSWORD_RESET_SUCCESS';
+export const ON_PASSWORD_RESET_FAILED = 'app/RegisterPage/ON_PASSWORD_RESET_FAILED';
