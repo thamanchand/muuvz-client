03a1160a7c45135a6a30f2028ced8d0e44fa81e9
Thaman <thaman.chand@invian.fi>
2020-05-08 10:47:14 +0545

    ON_PASSWORD_CHANGE, ON_PASSWORD_CHANGE_SUCCESS, ON_PASSWORD_CHANGE_FAILED reducers

--- app/containers/AccountPage/reducer.js
+++ app/containers/AccountPage/reducer.js
@@ -13,20 +13,27 @@ import {
 
 
 export const initialState = fromJS({
-
+  isPasswordChange: false,
+  isPasswordChangeLoading: false,
 });
 
 function accountPageReducer(state = initialState, action) {
   switch (action.type) {
     case ON_PASSWORD_CHANGE:
-      return state;
+      return state
+        .set('isPasswordChange', false)
+        .set('isPasswordChangeLoading', true);
 
     case ON_PASSWORD_CHANGE_SUCCESS:
-      return state;
+      return state
+        .set('isPasswordChange', true)
+        .set('isPasswordChangeLoading', false);
 
     case ON_PASSWORD_CHANGE_FAILED:
       return state
-        .set('error', fromJS(action.error))
+        .set('isPasswordChange', false)
+        .set('isPasswordChangeLoading', false)
+        .set('error', fromJS(action.error));
 
     default:
       return state;
