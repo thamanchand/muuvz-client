1802083e575241397c124bc41d3f0914fe94d10f
Thaman <thaman.chand@invian.fi>
2020-05-08 10:47:29 +0545

    isPasswordChangedSelector, isPasswordChangeLoadingSelector

--- app/containers/AccountPage/selector.js
+++ app/containers/AccountPage/selector.js
@@ -1,8 +1,22 @@
+import { createSelector } from 'reselect';
+
 import { initialState } from './reducer';
 
 /**
  * Account page
  */
 
-// ProfilePage domain
+// AccountPage domain
 export const selectAccountPageDomain = state => state.accountPage || initialState;
+
+export const isPasswordChangedSelector = () =>
+  createSelector(
+    selectAccountPageDomain,
+    subState => subState.get('isPasswordChange')
+  );
+
+export const isPasswordChangeLoadingSelector = () =>
+  createSelector(
+    selectAccountPageDomain,
+    subState => subState.get('isPasswordChangeLoading')
+  );
