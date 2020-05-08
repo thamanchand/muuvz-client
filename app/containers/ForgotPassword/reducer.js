5c8eb4cd979efadf7d5c3d24c8584a1a7f6cbaa6
Thaman <thaman.chand@invian.fi>
2020-05-08 09:11:04 +0545

    ON_PASSWORD_FORGOT, ON_PASSWORD_FORGOT_SUCCESS, ON_PASSWORD_FORGOT_FAILED state manage

---
+++ app/containers/ForgotPassword/reducer.js
@@ -0,0 +1,43 @@
+/*
+ *
+ * LoginPage reducer
+ *
+ */
+
+import { fromJS } from 'immutable';
+import {
+  ON_PASSWORD_FORGOT,
+  ON_PASSWORD_FORGOT_SUCCESS,
+  ON_PASSWORD_FORGOT_FAILED,
+} from './constants';
+
+
+export const initialState = fromJS({
+  isPasswordSendToEmail: false,
+  showLoader: false,
+});
+
+function passForgotPageReducer(state = initialState, action) {
+  switch (action.type) {
+    case ON_PASSWORD_FORGOT:
+      return state
+        .set('isPasswordSendToEmail', false)
+        .set('showLoader', true);
+
+    case ON_PASSWORD_FORGOT_SUCCESS:
+      return state
+        .set('isPasswordSendToEmail', true)
+        .set('showLoader', false);
+
+    case ON_PASSWORD_FORGOT_FAILED:
+      return state
+        .set('error', fromJS(action.error))
+        .set('isPasswordSendToEmail', false)
+        .set('showLoader', false);
+
+    default:
+      return state;
+  }
+}
+
+export default passForgotPageReducer;
