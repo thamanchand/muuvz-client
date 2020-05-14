a30c477801959c86e282431e9b9719ef99fc309e
Thaman <thaman.chand@invian.fi>
2020-05-14 17:07:09 +0545

    set isBookingAcceptloading, selectedBookingId reducer states

--- app/containers/Booking/reducer.js
+++ app/containers/Booking/reducer.js
@@ -13,6 +13,9 @@ import {
   ON_RESOURCE_LOAD,
   ON_RESOURCE_LOAD_SUCCESS,
   ON_RESOURCE_LOAD_FAILED,
+  ON_BOOKING_ACCEPT,
+  ON_BOOKING_ACCEPT_SUCCESS,
+  ON_BOOKING_ACCEPT_FAILED,
 } from './constants';
 
 
@@ -20,6 +23,8 @@ export const initialState = fromJS({
   bookingResult: [],
   resourceList: [],
   loading: false,
+  isBookingAcceptloading: false,
+  selectedBookingId: null,
 });
 
 function resourceBookingReducer(state = initialState, action) {
@@ -46,7 +51,23 @@ function resourceBookingReducer(state = initialState, action) {
         .set('isLoading', false);
 
     case ON_RESOURCE_LOAD_FAILED:
-      return state.set('error', fromJS(action.error))
+      return state.set('error', fromJS(action.error));
+
+    case ON_BOOKING_ACCEPT:
+      return state
+        .set('isBookingAcceptloading', true)
+        .set('selectedBookingId', fromJS(action.bookingId));
+
+    case ON_BOOKING_ACCEPT_SUCCESS:
+      return state
+        .set('isBookingAcceptloading', false)
+        .set('selectedBookingId', null);
+
+    case ON_BOOKING_ACCEPT_FAILED:
+      return state
+        .set('isBookingAcceptloading', false)
+        .set('selectedBookingId', null)
+        .set('error', fromJS(action.error));;
 
     default:
       return state;
