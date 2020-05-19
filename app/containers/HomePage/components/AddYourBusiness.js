d86b5b371e05c2c1bc2fbe070cf989cb40bf1b21
Thaman <thaman.chand@invian.fi>
2020-05-19 21:41:53 +0545

    AddYourBussiness CTA component

---
+++ app/containers/HomePage/components/AddYourBusiness.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const AddYourBussiness = () => (
+  <section className="section">
+    <div className="container">
+      <div className="bg-info-gradient rounded text-center p-5">
+        <h2 className="mb-5 addbusiness_h2">Try Muuvz now for free</h2>
+
+        <div className="scroll-down-link scroll-down-arrow" />
+        <div>
+          <button type="button" className="addyourbusiness__btn rounded">
+            ADD YOUR BUSINESS
+          </button>
+        </div>
+      </div>
+    </div>
+  </section>
+);
+
+export default AddYourBussiness;
