03e61734f51fab7d55b741031072857323ec32c0
Thaman <thaman.chand@invian.fi>
2020-05-19 23:17:00 +0545

    fix slide issue

--- app/containers/HomePage/components/BusinessBenefits.js
+++ app/containers/HomePage/components/BusinessBenefits.js
@@ -11,6 +11,12 @@ import calendar from '../../../assets/images/landing/calendar.svg';
 import resources from '../../../assets/images/landing/resources.svg';
 
 class BusinessBenefits extends React.Component {
+  constructor(props){
+  	super(props);
+  	this.state = {
+      swipeActiveIndex: 0,
+    };
+  }
 
   componentDidMount(){
     AOS.init({
@@ -19,8 +25,12 @@ class BusinessBenefits extends React.Component {
     });
   }
 
-  render() {
+  changeSlide = (slideIndex) => {
+    this.setState({swipeActiveIndex: slideIndex });
+  }
 
+  render() {
+    const { swipeActiveIndex } = this.state;
     return (
       <section className="section bg-gray">
         <div className="container">
@@ -28,18 +38,34 @@ class BusinessBenefits extends React.Component {
             <h3 className="landing__header-title benefits__header_margin">For Van owner</h3>
           </div>
           <nav className="slide nav nav-tabs nav-outlined nav-rounded justify-content-around justify-content-md-center mb-5">
-            <a className="nav-item nav-link active" data-toggle="tab" href="#dashboard-tab">
+            <span
+              type="button"
+              role="presentation"
+              className="nav-item nav-link active"
+              onClick={() => this.changeSlide(0)}
+            >
               Booking
-            </a>
-            <a className="nav-item nav-link" data-toggle="tab" href="#tasks-tab">
+
+            </span>
+            <span
+              type="button"
+              role="presentation"
+              className="nav-item nav-link"
+              onClick={() => this.changeSlide(1)}
+            >
               Calendar
-            </a>
-            <a className="nav-item nav-link" data-toggle="tab" href="#calendar-tab">
+            </span>
+            <span
+              type="button"
+              role="presentation"
+              className="nav-item nav-link"
+              onClick={() => this.changeSlide(2)}
+            >
               Vans
-            </a>
+            </span>
           </nav>
           <div className="tab-content">
-            <div className="tab-pane fade show active" id="dashboard-tab">
+            <div className={swipeActiveIndex === 0 ? "tab-pane fade show active" : "tab-pane fade"}>
               <div className="row gap-y align-items-center">
                 <div className="col-md-6 mr-md-auto">
                   <div className="browser shadow" data-aos="fade-right">
@@ -52,20 +78,20 @@ class BusinessBenefits extends React.Component {
                 </div>
               </div>
             </div>
-            <div className="tab-pane fade" id="tasks-tab">
+            <div className={swipeActiveIndex === 1 ? "tab-pane fade show active" : "tab-pane fade"}>
               <div className="row gap-y align-items-center">
-                <div className="col-md-6 order-md-2 ml-md-auto">
-                  <div className="browser shadow" data-aos="fade-left">
-                    <img src={calendar}  alt="" className="img-responsive"  />
+                <div className="col-md-6 mr-md-auto">
+                  <div className="browser shadow" data-aos="fade-right">
+                    <img src={calendar} className="img-responsive" alt="" />
                   </div>
                 </div>
-                <div className="col-md-5 mr-md-auto">
+                <div className="col-md-5">
                   <h3 className="landing__header-title">Calendar</h3>
-                  <p className="landing__feature-caption">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore maxime numquam perspiciatis saepe totam.</p>
+                  <p className="landing__feature-value">When buying Dashcore you will get right in your hand a powerful tool to boost your design. You will get not just a template but a complete ready-to-use HTML code snippets to start right away with your project.</p>
                 </div>
               </div>
             </div>
-            <div className="tab-pane fade" id="calendar-tab">
+            <div className={swipeActiveIndex === 2 ? "tab-pane fade show active" : "tab-pane fade"}>
               <div className="row gap-y align-items-center">
                 <div className="col-md-6 mr-md-auto">
                   <div className="browser shadow mt-4 mt-md-0 aos-init aos-animate" data-aos="fade-right">
