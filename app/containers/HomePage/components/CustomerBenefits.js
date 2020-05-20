a124fd6d8ce6d7f7259436ac661757aa09797086
Thaman <thaman.chand@invian.fi>
2020-05-20 14:20:29 +0545

    hide nav on small device

--- app/containers/HomePage/components/CustomerBenefits.js
+++ app/containers/HomePage/components/CustomerBenefits.js
@@ -42,47 +42,47 @@ class CusotmerBenefits extends React.Component {
           </div>
           <div className="row align-items-center">
             <div className="col-lg-4">
-              <h4 className="landing__feature-caption">Book van in no time... just in 3 steps.</h4>
-
-              <ol id="sw-nav-tools" className="nav nav-process nav-circle nav-justified mt-5">
-                <li className={swipeActiveIndex === 0 ? "nav-item active" : "nav-item" }>
-                  <span
-                    role="presentation"
-                    className="nav-link"
-                    data-step="1"
-                    onClick={() => this.changeSwiper(0)}
-                  >
-                    <small className="nav__process-label absolute">Search</small>
-                  </span>
-                </li>
-                <li className={swipeActiveIndex === 1 ? "nav-item active" : "nav-item"}>
-                  <span
-                    role="presentation"
-                    className="nav-link"
-                    data-step="2"
-                    onClick={() => this.changeSwiper(1)}
-                  >
-                    <small className="nav__process-label absolute">Compare</small>
-                  </span>
-                </li>
-                <li className={swipeActiveIndex === 2 ? "nav-item active" : "nav-item"}>
-                  <span
-                    role="presentation"
-                    className="nav-link"
-                    data-step="3"
-                    onClick={() => this.changeSwiper(2)}
-                  >
-                    <small className="nav__process-label absolute">Book</small>
-                  </span>
-                </li>
-
-              </ol>
+              <h4 className="landing__feature-caption customer_value_title">Book van in no time... just in 3 steps.</h4>
+              <div className="customer_benefits_nav">
+                <ol id="sw-nav-tools" className="nav nav-process nav-circle nav-justified mt-5 align-items-center">
+                  <li className={swipeActiveIndex === 0 ? "nav-item active" : "nav-item" }>
+                    <span
+                      role="presentation"
+                      className="nav-link"
+                      data-step="1"
+                      onClick={() => this.changeSwiper(0)}
+                    >
+                      <small className="nav__process-label absolute">Search</small>
+                    </span>
+                  </li>
+                  <li className={swipeActiveIndex === 1 ? "nav-item active" : "nav-item"}>
+                    <span
+                      role="presentation"
+                      className="nav-link"
+                      data-step="2"
+                      onClick={() => this.changeSwiper(1)}
+                    >
+                      <small className="nav__process-label absolute">Compare</small>
+                    </span>
+                  </li>
+                  <li className={swipeActiveIndex === 2 ? "nav-item active" : "nav-item"}>
+                    <span
+                      role="presentation"
+                      className="nav-link"
+                      data-step="3"
+                      onClick={() => this.changeSwiper(2)}
+                    >
+                      <small className="nav__process-label absolute">Book</small>
+                    </span>
+                  </li>
+                </ol>
+              </div>
             </div>
             <div className="col-lg-7 ml-lg-auto">
               <div className="browser shadow mt-4 mt-md-0" data-aos="fade-left">
                 <div className="swiper-container">
                   <div className="swiper-wrapper">
-                    <div className="swiper-slide"><img src={search} alt="" className="img-responsive" /></div>
+                    <div className="swiper-slide align-items-center"><img src={search} alt="" className="img-responsive" /></div>
                     <div className="swiper-slide"><img src={compare} alt="" className="img-responsive" /></div>
                     <div className="swiper-slide"><img src={book}  alt="" className="img-responsive" /></div>
                   </div>
