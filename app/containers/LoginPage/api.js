c494b7aa568571b0fba4ea7980fabf5cf11ef157
Thaman <thaman.chand@invian.fi>
2020-05-21 15:42:50 +0545

    refreshToken api util added

--- app/containers/LoginPage/api.js
+++ app/containers/LoginPage/api.js
@@ -1,11 +1,20 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:1337/auth/local';
+const BASE_URL = 'http://localhost:1337/';
 
 // Login user
 export const loginUser = body => axios
   .post(
-    BASE_URL,
+    `${BASE_URL}${'auth/local'}`,
+    { ...body },
+  ).then(response => response.data)
+  .catch(error => {
+    throw error;
+  });
+
+export const refreshToken = body => axios
+  .post(
+    `${BASE_URL}${'users-permissions/refreshToken'}`,
     { ...body },
   ).then(response => response.data)
   .catch(error => {
