5fda8482585aa649dd7357a85498278b22a97879
Thaman <thaman.chand@invian.fi>
2020-05-21 12:06:34 +0545

    add more CTA message

--- app/containers/HomePage/components/AddYourBusiness.js
+++ app/containers/HomePage/components/AddYourBusiness.js
@@ -4,8 +4,8 @@ const AddYourBussiness = () => (
   <section className="section">
     <div className="container">
       <div className="bg-info-gradient rounded text-center p-5">
-        <h2 className="mb-5 addbusiness_h2">Try Muuvz now for free</h2>
-
+        <h2 className="mb-5 addbusiness_h2">Increase bookings now!</h2>
+        <p className="addbusiness_text">Spend less time managing booking and more time growing your business.</p>
         <div className="scroll-down-link scroll-down-arrow" />
         <div>
           <button type="button" className="addyourbusiness__btn rounded">
