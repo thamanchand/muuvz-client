7f4e6e102591737eb9f2aee1d81a4b49d9b85b0c
Thaman <thaman.chand@invian.fi>
2020-07-02 12:06:29 +0545

    store userInfo in session, redirect to diff url based on if user isBusiness or profileCompleted

--- app/containers/ConnectPage/saga.js
+++ app/containers/ConnectPage/saga.js
@@ -2,15 +2,21 @@ import {
   all,
   call,
   take,
+  fork,
   takeLatest,
+  put,
+  cancel,
 } from 'redux-saga/effects';
 import { LOCATION_CHANGE } from 'react-router-redux';
-
 
 // Utils
 import auth from 'utils/auth';
 import request from 'utils/request';
 import { history } from '../../utils/history';
+
+import { onLoginSubmitSuccess, onLoginSubmitFailed } from '../LoginPage/actions';
+
+import toast from '../../shared/ToastNotify';
 
 // Constants
 import { LOG_USER } from './constants';
@@ -18,18 +24,58 @@ import { LOG_USER } from './constants';
 export function* login(action) {
   try {
     const requestURL = `http://localhost:1337/auth/${action.provider}/callback${action.search}`;
-    const response = yield call(request, requestURL, { method: 'GET' });
+    const isLoggedFromListingPage = action.loginSource === 'listingPage';
 
+    const response = yield call(request, requestURL, { method: 'GET' });
     if (response.jwt) {
+
+      auth.clearToken();
+      const { email, confirmed, profileCompleted, isbusiness, id } = response.user;
+      const userFieldsLocallyStored = { email, confirmed, profileCompleted, isbusiness, id}
+      // auth.clearAppStorage();
+
       // Set the user's credentials
       yield all([
-        call(auth.setToken, response.jwt, true),
-        call(auth.setUserInfo, response.user, true),
+        call(auth.setToken, response.jwt),
+        call(auth.setUserInfo, userFieldsLocallyStored),
       ]);
-      yield call(forwardTo, '/');
+      const isBusiness = auth.get('userInfo').isbusiness;
+      const isProfileCompleted = auth.get('userInfo').profileCompleted;
+      if (isLoggedFromListingPage) {
+        toast.success("Logged in successfully!");
+        yield put(onLoginSubmitSuccess());
+        yield call(forwardTo, `${'/listing?loginSuccess'}`);
+        // const submitWatcher = yield fork(takeLatest, ON_LOGIN_SUBMIT, submitForm);
+      }
+      else if (isBusiness && isProfileCompleted) {
+        yield put(onLoginSubmitSuccess());
+        yield call(forwardTo, '/dashboard/booking');
+        toast.success("Logged in successfully!");
+        const submitWatcher = yield fork(takeLatest, LOG_USER, login);
+        yield cancel(submitWatcher);
+      } else if (isBusiness && !isProfileCompleted) {
+        yield put(onLoginSubmitSuccess());
+        yield call(forwardTo, '/dashboard/profile');
+        toast.success("Logged in successfully!");
+        const submitWatcher = yield fork(takeLatest, LOG_USER, login);
+        yield cancel(submitWatcher);
+      } else if (!isBusiness && !isProfileCompleted) {
+        yield put(onLoginSubmitSuccess());
+        yield call(forwardTo, '/dashboard/profile');
+        toast.success("Logged in successfully!");
+        const submitWatcher = yield fork(takeLatest, LOG_USER, login);
+        yield cancel(submitWatcher);
+      } else if (!isLoggedFromListingPage && !isBusiness) {
+        yield put(onLoginSubmitSuccess());
+        yield call(forwardTo, '/');
+        toast.success("Logged in successfully!");
+        const submitWatcher = yield fork(takeLatest, LOG_USER, login);
+        yield cancel(submitWatcher);
+      }
     }
   } catch(error) {
     yield call(forwardTo, '/auth/login');
+    yield put(onLoginSubmitFailed(error));
   }
 }
 
