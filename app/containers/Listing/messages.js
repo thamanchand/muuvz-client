69f6b48d6456a31bb4f55886ce2b4bd51597bc9d
Thaman <thaman.chand@invian.fi>
2020-07-04 12:36:01 +0545

    Listing component add messages.js

---
+++ app/containers/Listing/messages.js
@@ -0,0 +1,43 @@
+/*
+ * Search componebt Messages
+ *
+ * This contains all the text for the Search component.
+ */
+import { defineMessages } from 'react-intl';
+
+export const scope = 'muuvz.component.VanListing';
+
+export default defineMessages({
+  searchPlace: {
+    id: `${scope}.searchPlace`,
+    defaultMessage: 'City',
+  },
+  searchPickupDate: {
+    id: `${scope}.searchPickupDate`,
+    defaultMessage: 'Pickup date',
+  },
+  searchPickupTime: {
+    id: `${scope}.searchPickupTime`,
+    defaultMessage: 'Pickup time',
+  },
+  searchDropOffDate: {
+    id: `${scope}.searchDropOffDate`,
+    defaultMessage: 'Drop-off date',
+  },
+  searchDropOffTime: {
+    id: `${scope}.searchDropOffTime`,
+    defaultMessage: 'Drop-off time',
+  },
+  selectDateTimeNotify: {
+    id: `${scope}.selectDateTimeNotify`,
+    defaultMessage: 'Please select both date and time',
+  },
+  lessThan2HrsNotify: {
+    id: `${scope}.lessThan2HrsNotify`,
+    defaultMessage: 'You can not book less than 2 hrs',
+  },
+  searchVansBtn: {
+    id: `${scope}.searchVansBtn`,
+    defaultMessage: 'Search vans',
+  },
+});
